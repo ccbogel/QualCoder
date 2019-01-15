9e25e109c7e6d57e23e2f9718962c91c4f4efe44
Colin Curtain <ccbogel@hotmail.com>
2019-01-15 21:35:59 +1100

    install.sh and QualCoder.desktop for linux launcher
    Files to add a Linux launcher for QualCoder. The directory above QualCoder should have install.sh and QualCoder.desktop. Run install.sh to install the Launcher. The QualCoder directory will be copied to the /usr/share directory. Once copied, it should work. if not, ensure all users can use the files: e.g. run in terminal in /usr/share: sudo chmod -R 755 QualCoder

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "QualCoder will be copied to the directory /usr/share/"
+echo "These actions require owner (sudo) permission"
+
+sudo cp -r QualCoder /usr/share/QualCoder
+sudo cp QualCoder/GUI/QualCoder.png /usr/share/pixmaps/QualCoder.png
+sudo cp QualCoder.desktop /usr/share/applications/QualCoder.desktop
+
+echo "Completed"
+echo "You may need to install pyqt5 and optionally pypdf"
+echo "On Linux:"
+echo "sudo pip3 install pyqt5"
+echo "sudo pip3 install pypdf"
+echo "On Windows:"
+echo "pip install pyqt5"
+echo "pip install pypdf"
+echo "To remove QualCoder from Linux run the following in the terminal:"
+echo "sudo rm -R /usr/share/QualCoder"
+echo "sudo rm /usr/share/pixmaps/QualCoder.png"
