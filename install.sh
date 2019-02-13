fd74e2dc6e34a6d34daff47990dcdbcc34050eea
Colin Curtain <ccbogel@hotmail.com>
2019-02-13 11:21:03 +1100

    install also try to install pyqt5 and lxml
    Made the installed do more python module importing, so that end users do not have as much to do to resolve dependencies.

--- install.sh
+++ install.sh
@@ -2,17 +2,13 @@
 
 echo "QualCoder will be copied to the directory /usr/share/"
 echo "These actions require owner (sudo) permission"
-
+echo "The installer will also try to install pyqt5 and lxml"
 sudo cp -r QualCoder /usr/share/QualCoder
 sudo cp QualCoder/GUI/QualCoder.png /usr/share/pixmaps/QualCoder.png
 sudo cp QualCoder.desktop /usr/share/applications/QualCoder.desktop
-
+sudo apt-get install python3-pyqt5
+sudo apt-get install python3-lxml
 echo "Installation completed."
-echo "You may need to install pyqt5."
-echo "On Linux:"
-echo "sudo pip3 install pyqt5"
-echo "On Windows:"
-echo "pip install pyqt5"
 echo "To remove QualCoder from Linux run the following in the terminal:"
 echo "sudo rm -R /usr/share/QualCoder"
 echo "sudo rm /usr/share/pixmaps/QualCoder.png"
