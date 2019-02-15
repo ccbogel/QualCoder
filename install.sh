0a0709c06ba44224f430f6d0c282e561af9db930
Colin Curtain <ccbogel@hotmail.com>
2019-02-15 16:16:53 +1100

    some updates
    Manual updated for Windows preparatory modules download instructions. 
    Linux install.sh updated to include import of pil Currently Pillow is not essential for QualCoder, but it is a dependency for the docx.py module.

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+echo "This installer is for Linux installations only"
 echo "QualCoder will be copied to the directory /usr/share/"
 echo "These actions require owner (sudo) permission"
 echo "The installer will also try to install pyqt5 and lxml"
+sudo apt-get install python3-pyqt5
+sudo apt-get install python3-lxml
+sudo apt-get install python3-pil
 sudo cp -r QualCoder /usr/share/QualCoder
 sudo cp QualCoder/GUI/QualCoder.png /usr/share/pixmaps/QualCoder.png
 sudo cp QualCoder.desktop /usr/share/applications/QualCoder.desktop
-sudo apt-get install python3-pyqt5
-sudo apt-get install python3-lxml
 echo "Installation completed."
 echo "To remove QualCoder from Linux run the following in the terminal:"
 echo "sudo rm -R /usr/share/QualCoder"
