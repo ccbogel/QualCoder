e8edbcee52a732a25dfe07e0e30d773b1f2ecf7d
Colin Curtain <ccbogel@hotmail.com>
2019-04-09 23:01:49 +1000

    added language combobox

--- QualCoder/GUI/ui_dialog_settings.py
+++ QualCoder/GUI/ui_dialog_settings.py
@@ -2,8 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui_dialog_settings.ui'
 #
-# Created: Mon Dec 18 08:25:32 2017
-#      by: PyQt5 UI code generator 5.2.1
+# Created by: PyQt5 UI code generator 5.5.1
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -23,6 +22,7 @@ class Ui_Dialog_settings(object):
         self.lineEdit_coderName.setObjectName("lineEdit_coderName")
         self.label_coderName = QtWidgets.QLabel(Dialog_settings)
         self.label_coderName.setGeometry(QtCore.QRect(40, 30, 141, 21))
+        self.label_coderName.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label_coderName.setObjectName("label_coderName")
         self.fontComboBox = QtWidgets.QFontComboBox(Dialog_settings)
         self.fontComboBox.setGeometry(QtCore.QRect(30, 180, 229, 38))
@@ -37,7 +37,7 @@ class Ui_Dialog_settings(object):
         self.label.setGeometry(QtCore.QRect(30, 150, 211, 21))
         self.label.setObjectName("label")
         self.checkBox = QtWidgets.QCheckBox(Dialog_settings)
-        self.checkBox.setGeometry(QtCore.QRect(30, 120, 151, 22))
+        self.checkBox.setGeometry(QtCore.QRect(410, 180, 151, 22))
         self.checkBox.setObjectName("checkBox")
         self.label_directory = QtWidgets.QLabel(Dialog_settings)
         self.label_directory.setGeometry(QtCore.QRect(30, 310, 671, 21))
@@ -46,10 +46,11 @@ class Ui_Dialog_settings(object):
         self.pushButton_choose_directory.setGeometry(QtCore.QRect(30, 270, 391, 31))
         self.pushButton_choose_directory.setObjectName("pushButton_choose_directory")
         self.label_2 = QtWidgets.QLabel(Dialog_settings)
-        self.label_2.setGeometry(QtCore.QRect(35, 80, 141, 20))
+        self.label_2.setGeometry(QtCore.QRect(35, 77, 141, 20))
+        self.label_2.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label_2.setObjectName("label_2")
         self.comboBox_coders = QtWidgets.QComboBox(Dialog_settings)
-        self.comboBox_coders.setGeometry(QtCore.QRect(200, 70, 241, 33))
+        self.comboBox_coders.setGeometry(QtCore.QRect(200, 67, 241, 33))
         self.comboBox_coders.setObjectName("comboBox_coders")
         self.label_3 = QtWidgets.QLabel(Dialog_settings)
         self.label_3.setGeometry(QtCore.QRect(30, 220, 241, 41))
@@ -61,11 +62,26 @@ class Ui_Dialog_settings(object):
         self.spinBox_treefontsize.setMaximum(32)
         self.spinBox_treefontsize.setSingleStep(2)
         self.spinBox_treefontsize.setObjectName("spinBox_treefontsize")
+        self.label_4 = QtWidgets.QLabel(Dialog_settings)
+        self.label_4.setGeometry(QtCore.QRect(45, 120, 131, 20))
+        self.label_4.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.label_4.setObjectName("label_4")
+        self.comboBox_language = QtWidgets.QComboBox(Dialog_settings)
+        self.comboBox_language.setGeometry(QtCore.QRect(200, 110, 241, 33))
+        self.comboBox_language.setObjectName("comboBox_language")
 
         self.retranslateUi(Dialog_settings)
         self.buttonBox.accepted.connect(Dialog_settings.accept)
         self.buttonBox.rejected.connect(Dialog_settings.reject)
         QtCore.QMetaObject.connectSlotsByName(Dialog_settings)
+        Dialog_settings.setTabOrder(self.lineEdit_coderName, self.comboBox_coders)
+        Dialog_settings.setTabOrder(self.comboBox_coders, self.comboBox_language)
+        Dialog_settings.setTabOrder(self.comboBox_language, self.fontComboBox)
+        Dialog_settings.setTabOrder(self.fontComboBox, self.spinBox)
+        Dialog_settings.setTabOrder(self.spinBox, self.spinBox_treefontsize)
+        Dialog_settings.setTabOrder(self.spinBox_treefontsize, self.checkBox)
+        Dialog_settings.setTabOrder(self.checkBox, self.pushButton_choose_directory)
+        Dialog_settings.setTabOrder(self.pushButton_choose_directory, self.buttonBox)
 
     def retranslateUi(self, Dialog_settings):
         _translate = QtCore.QCoreApplication.translate
@@ -77,6 +93,8 @@ class Ui_Dialog_settings(object):
         self.pushButton_choose_directory.setText(_translate("Dialog_settings", "Default project directory"))
         self.label_2.setText(_translate("Dialog_settings", "Coders"))
         self.label_3.setText(_translate("Dialog_settings", "Font size for categories and codes tree"))
+        self.label_4.setText(_translate("Dialog_settings", "Language"))
+        self.comboBox_language.setToolTip(_translate("Dialog_settings", "<html><head/><body><p>Close and open the software for the change in language to occur.</p></body></html>"))
 
 
 if __name__ == "__main__":
