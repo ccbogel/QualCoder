93880282b851a02368841a7e4f2f506fad3db961
Colin Curtain <ccbogel@hotmail.com>
2019-04-15 18:01:35 +1000

    updated
    using the lowercase qualcoder folder now

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "This installer is for Linux installations only"
-echo "QualCoder will be copied to the directory /usr/share/"
+echo "qualcoder will be copied to the directory /usr/share/"
 echo "These actions require owner (sudo) permission"
-echo "The installer will also try to install pyqt5 and lxml"
-sudo apt-get install python3-pyqt5
-sudo apt-get install python3-lxml
-sudo apt-get install python3-pil
-sudo cp -r QualCoder /usr/share/QualCoder
-sudo cp QualCoder/GUI/QualCoder.png /usr/share/pixmaps/QualCoder.png
-sudo cp QualCoder.desktop /usr/share/applications/QualCoder.desktop
+echo "The installer will also install dependencies"
+sudo apt install python3-pyqt5 python3-lxml python3-pil vlc qtwayland5 qpdf
+# sudo apt install libavcodec-extra  # for streaming and transcoding
+sudo cp -r qualcoder /usr/share/qualcoder
+sudo cp qualcoder/GUI/QualCoder.png /usr/share/pixmaps/QualCoder.png
+sudo cp qualcoder.desktop /usr/share/applications/qualcoder.desktop
 echo "Installation completed."
-echo "To remove QualCoder from Linux run the following in the terminal:"
-echo "sudo rm -R /usr/share/QualCoder"
+echo "To remove qualcoder from Linux run the following in the terminal:"
+echo "sudo rm -R /usr/share/qualcoder"
 echo "sudo rm /usr/share/pixmaps/QualCoder.png"
+echo "sudo rm /usr/share/applications/qualcoder.desktop"
