5ffea0fb5daf6384720405394152ee2b999f12f1
Colin Curtain <ccbogel@hotmail.com>
2019-04-16 07:34:16 +1000

    minor changes, moved .desktop to qualcoder/gui

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo apt install python3-pyqt5 python3-lxml python3-pil vlc qtwayland5 qpdf
 # sudo apt install libavcodec-extra  # for streaming and transcoding
 sudo cp -r qualcoder /usr/share/qualcoder
 sudo cp qualcoder/GUI/QualCoder.png /usr/share/pixmaps/QualCoder.png
-sudo cp qualcoder.desktop /usr/share/applications/qualcoder.desktop
+sudo cp qualcoder/GUI/qualcoder.desktop /usr/share/applications/qualcoder.desktop
 echo "Installation completed."
 echo "To remove qualcoder from Linux run the following in the terminal:"
 echo "sudo rm -R /usr/share/qualcoder"
