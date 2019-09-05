2d33c4f13b01bd74b7f3670232cab939c436ac1b
Colin Curtain <ccbogel@hotmail.com>
2019-09-06 08:03:11 +1000

    Extra modules instillation

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ echo "This installer is for Linux installations only"
 echo "qualcoder will be copied to the directory /usr/share/"
 echo "These actions require owner (sudo) permission"
 echo "The installer will also install dependencies"
-sudo apt install python3-pyqt5 python3-lxml python3-pil vlc qtwayland5 qpdf
+sudo apt install python3-pip python3-pyqt5 python3-lxml python3-pil vlc qtwayland5 qpdf python3-ebooklib python3-ply python3-six python3-chardet python3-click
+sudo pip3 install pikepdf pdfminer.six
 # sudo apt install libavcodec-extra  # for streaming and transcoding
 sudo cp -r qualcoder /usr/share/qualcoder
 sudo cp qualcoder/GUI/QualCoder.png /usr/share/pixmaps/QualCoder.png
