aa69a72315d20883d0a869a14434f5438b55669d
Martin Ortbauer <mortbauer@gmail.com>
2019-09-08 17:52:44 +0200

    change to use dh-virtualenv for debian

---
+++ build_in_docker.sh
@@ -0,0 +1,10 @@
+container=$(docker run --rm -it -d ubuntu-dev bash)
+name=$(python setup.py sdist | grep removing | cut -d' ' -f2 | sed "s/'//g")
+docker cp dist/${name}.tar.gz ${container}:/
+docker exec ${container} tar xf /${name}.tar.gz
+docker cp debian ${container}:/${name}
+#docker exec ${container} sh -c "python3 -m venv venv && venv/bin/pip install -r ${name}/requirements.txt"
+docker exec ${container} sh -c "cd ${name} && dpkg-buildpackage -us -uc -b && mkdir /dist && cp ../*.deb /dist"
+docker cp ${container}:/dist .
+docker exec -it ${container} bash
+docker container stop ${container}
