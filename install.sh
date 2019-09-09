f1e261c3eb7c9f0a56f1f255d898dc3bac7ea47f
Colin Curtain <ccbogel@hotmail.com>
2019-09-09 10:14:14 +1000

    Add files via upload

--- install.sh
+++ install.sh
@@ -4,13 +4,12 @@ echo "This installer is for Linux installations only"
 echo "qualcoder will be copied to the directory /usr/share/"
 echo "These actions require owner (sudo) permission"
 echo "The installer will also install dependencies"
-sudo apt install python3-pip python3-pyqt5 python3-lxml python3-pil vlc qpdf python3-ebooklib python3-ply python3-six python3-chardet python3-click
-sudo pip install pdfminer.six
-sudo pip install pikepdf
-# sudo apt install libavcodec-extra  # for streaming and transcoding
+sudo apt install python3-pip python3-pyqt5 python3-lxml python3-pil vlc qtwayland5 qpdf python3-ebooklib python3-ply python3-six python3-chardet python3-click
+echo "Please wait ..."
+sudo pip3 install pikepdf pdfminer.six
 sudo cp -r qualcoder /usr/share/qualcoder
-sudo cp qualcoder.png /usr/share/pixmaps/qualcoder.png
-sudo cp qualcoder.desktop /usr/share/applications/qualcoder.desktop
+sudo cp qualcoder/GUI/qualcoder.png /usr/share/pixmaps/qualcoder.png
+sudo cp qualcoder/GUI/qualcoder.desktop /usr/share/applications/qualcoder.desktop
 echo "Installation completed."
 echo "To remove qualcoder from Linux run the following in the terminal:"
 echo "sudo rm -R /usr/share/qualcoder"
