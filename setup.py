539f9fa1139206e0e89447247205c1d1561dcc60
Colin Curtain <ccbogel@hotmail.com>
2020-03-07 10:38:31 +1100

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,48 +1,46 @@
-from setuptools import setup, find_packages
+""" from setup example: https://github.com/pypa/sampleproject/blob/master/setup.py
+"""
 
-def readme():
-    with open('README') as f:
-        return f.read()
+from setuptools import setup, find_packages
+from os import path
+here = path.abspath(path.dirname(__file__))
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    setup_requires=["wheel"],
     name='qualcoder',
-    version='1.4.1',
+    version='1.8',
+    url='http://github.com/ccbogel/QualCoder',
+    author='Colin Curtain',
+    author_email='ccbogel@hotmail.com',
     description='Qualitative data analysis',
-    long_description=readme(),
+    long_description=long_description,
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha'
     ],
-    entry_points = {
-        'gui_scripts' : ['qualcoder = qualcoder.qualcoder:gui'],
-        'console_scripts' : ['qualcoder-cli = qualcoder.qualcoder:cli'],
-    },
-    # data_files = [
-        # ('share/applications/', ['qualcoder.desktop'])
-    # ],
-    packages=find_packages(),
-    package_data={'qualcoder': ['*.xsd', 'GUI/*.html', 'GUI/NotoSans-hinted/*.ttf']},
     keywords='qualitative data analysis',
-    url='http://github.com/ccbogel/QualCoder',
-    author='Colin Curtain',
-    author_email='ccbogel@hotmail.com',
-    license='MIT',
-    # install_requires=[
-        # 'graphviz',
-        # 'pyqt5',
-        # 'lxml',
-        # 'Pillow', 
-        # 'pikepdf', 
-        # 'six', 
-        # 'ebooklib',
-        # 'pdfminer.six',
-        # 'ply',
-        # 'chardet',
-        # 'python-vlc',
-        # 'click',
-    # ],
-    include_package_data=True,
+    packages=find_packages(where='qualcoder'),
+    python_requires='>=3.5',
+    install_requires=[
+        'pyqt5',
+        'lxml',
+        'Pillow', 
+        'ebooklib',
+        'pdfminer.six',
+        'ply',
+        'chardet'
+    ],
+    package_data={
+        'qualcoder':['Codebook.xsd', 'Project-mrt2019.xsd',
+        'GUI/*.html', 'GUI/NotoSans-hinted/*.ttf',
+        'locale/de/app_de.qm', 'locale/de/LC_MESSAGES/de,mo',
+        'locale/es/app_es.qm', 'locale/es/LC_MESSAGES/es,mo',
+        'locale/de/app_fr.qm', 'locale/fr/LC_MESSAGES/fr,mo',
+        'locale/en/LC_MESSAGES/en,mo',]
+    },
     zip_safe=False,
 )
