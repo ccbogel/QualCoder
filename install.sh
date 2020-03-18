d6fad4f8947637e63af5c5b9b20c42bbab5ac8e5
Colin Curtain <ccbogel@hotmail.com>
2020-03-19 08:10:32 +1100

    user information now states for Debian-based systems

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "This installer is for Linux installations only"
+echo "This installer is for Debian-based Linux installations only"
 echo "qualcoder will be copied to the directory /usr/share/"
 echo "These actions require owner (sudo) permission"
 echo "The installer will also install dependencies"
 sudo apt install python3-pip python3-pyqt5 python3-lxml python3-pil vlc python3-ebooklib python3-ply python3-six python3-chardet
 echo "Please wait ..."
-sudo python3 -m pip install pikepdf pdfminer.six
+sudo python3 -m pip install pdfminer.six
 sudo cp -r qualcoder /usr/share/qualcoder
 sudo cp qualcoder/GUI/qualcoder.png /usr/share/pixmaps/qualcoder.png
 sudo cp qualcoder/GUI/qualcoder.desktop /usr/share/applications/qualcoder.desktop
