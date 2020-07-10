6fe9ec2a7197af773ed704709b2e766e504b2241
Colin Curtain <ccbogel@hotmail.com>
2020-07-10 10:51:03 +1000

    includes python3-xlrd

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo "This installer uses apt package management"
 echo "QualCoder will be copied to the directory /usr/share/"
 echo "These actions require owner (sudo) permission"
 echo "The installer will also install dependencies"
-sudo apt install python3-pip python3-pyqt5 python3-lxml python3-pil vlc python3-ebooklib python3-ply python3-six python3-chardet
+sudo apt install python3-pip python3-pyqt5 python3-lxml python3-pil vlc python3-ebooklib python3-ply python3-six python3-chardet python3-xlrd
 echo "Please wait ..."
 sudo python3 -m pip install pdfminer.six
 sudo cp -r qualcoder /usr/share/qualcoder
