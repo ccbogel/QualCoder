d51c5bd466d4338476fb7318f673bf03674336cd
Colin Curtain <ccbogel@hotmail.com>
2020-08-04 22:15:27 +1000

    Add files via upload

--- qualcoder/GUI/ui_dialog_report_code_frequencies.py
+++ qualcoder/GUI/ui_dialog_report_code_frequencies.py
@@ -18,8 +18,8 @@ class Ui_Dialog_reportCodeFrequencies(object):
         self.verticalLayout = QtWidgets.QVBoxLayout(Dialog_reportCodeFrequencies)
         self.verticalLayout.setObjectName("verticalLayout")
         self.groupBox = QtWidgets.QGroupBox(Dialog_reportCodeFrequencies)
-        self.groupBox.setMinimumSize(QtCore.QSize(0, 60))
-        self.groupBox.setMaximumSize(QtCore.QSize(16777215, 60))
+        self.groupBox.setMinimumSize(QtCore.QSize(0, 90))
+        self.groupBox.setMaximumSize(QtCore.QSize(16777215, 90))
         self.groupBox.setTitle("")
         self.groupBox.setObjectName("groupBox")
         self.label_selections = QtWidgets.QLabel(self.groupBox)
@@ -34,6 +34,9 @@ class Ui_Dialog_reportCodeFrequencies(object):
         self.pushButton_exportcsv = QtWidgets.QPushButton(self.groupBox)
         self.pushButton_exportcsv.setGeometry(QtCore.QRect(368, 30, 291, 25))
         self.pushButton_exportcsv.setObjectName("pushButton_exportcsv")
+        self.pushButton_select_files = QtWidgets.QPushButton(self.groupBox)
+        self.pushButton_select_files.setGeometry(QtCore.QRect(10, 60, 271, 25))
+        self.pushButton_select_files.setObjectName("pushButton_select_files")
         self.verticalLayout.addWidget(self.groupBox)
         self.groupBox_2 = QtWidgets.QGroupBox(Dialog_reportCodeFrequencies)
         self.groupBox_2.setTitle("")
@@ -55,6 +58,7 @@ class Ui_Dialog_reportCodeFrequencies(object):
         self.label_selections.setText(_translate("Dialog_reportCodeFrequencies", "Code and category frequencies: overall and by coder"))
         self.pushButton_exporttext.setText(_translate("Dialog_reportCodeFrequencies", "Export text file"))
         self.pushButton_exportcsv.setText(_translate("Dialog_reportCodeFrequencies", "Export csv file"))
+        self.pushButton_select_files.setText(_translate("Dialog_reportCodeFrequencies", "Select files"))
 
 
 if __name__ == "__main__":
