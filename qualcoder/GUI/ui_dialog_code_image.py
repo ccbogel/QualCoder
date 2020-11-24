dc0fa77ee4d99c2fc58e396bc7882cb400ba22b6
Colin Curtain <ccbogel@hotmail.com>
2020-11-25 09:01:48 +1100

    Add files via upload

--- qualcoder/GUI/ui_dialog_code_image.py
+++ qualcoder/GUI/ui_dialog_code_image.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ui_dialog_code_image2.ui'
+# Form implementation generated from reading ui file 'ui_dialog_code_image.ui'
 #
-# Created: Thu Jan 25 08:07:02 2018
-#      by: PyQt5 UI code generator 5.2.1
+# Created by: PyQt5 UI code generator 5.14.1
 #
 # WARNING! All changes made in this file will be lost!
 
+
 from PyQt5 import QtCore, QtGui, QtWidgets
+
 
 class Ui_Dialog_code_image(object):
     def setupUi(self, Dialog_code_image):
@@ -30,13 +31,10 @@ class Ui_Dialog_code_image(object):
         self.groupBox_2.setTitle("")
         self.groupBox_2.setObjectName("groupBox_2")
         self.pushButton_memo = QtWidgets.QPushButton(self.groupBox_2)
-        self.pushButton_memo.setGeometry(QtCore.QRect(20, 40, 141, 32))
+        self.pushButton_memo.setGeometry(QtCore.QRect(380, 7, 141, 32))
         self.pushButton_memo.setObjectName("pushButton_memo")
-        self.pushButton_select = QtWidgets.QPushButton(self.groupBox_2)
-        self.pushButton_select.setGeometry(QtCore.QRect(10, 0, 201, 32))
-        self.pushButton_select.setObjectName("pushButton_select")
         self.label_coder = QtWidgets.QLabel(self.groupBox_2)
-        self.label_coder.setGeometry(QtCore.QRect(360, 10, 301, 21))
+        self.label_coder.setGeometry(QtCore.QRect(10, 10, 301, 21))
         self.label_coder.setObjectName("label_coder")
         self.checkBox_show_coders = QtWidgets.QCheckBox(self.groupBox_2)
         self.checkBox_show_coders.setGeometry(QtCore.QRect(690, 10, 221, 22))
@@ -49,23 +47,29 @@ class Ui_Dialog_code_image(object):
         self.groupBox.setTitle("")
         self.groupBox.setObjectName("groupBox")
         self.gridLayout_2 = QtWidgets.QGridLayout(self.groupBox)
-        self.gridLayout_2.setContentsMargins(0, 0, 0, 0)
-        self.gridLayout_2.setSpacing(0)
         self.gridLayout_2.setObjectName("gridLayout_2")
         self.splitter = QtWidgets.QSplitter(self.groupBox)
-        self.splitter.setOrientation(QtCore.Qt.Horizontal)
+        self.splitter.setOrientation(QtCore.Qt.Vertical)
         self.splitter.setObjectName("splitter")
+        self.listWidget = QtWidgets.QListWidget(self.splitter)
+        self.listWidget.setObjectName("listWidget")
         self.treeWidget = QtWidgets.QTreeWidget(self.splitter)
         self.treeWidget.setObjectName("treeWidget")
         self.treeWidget.headerItem().setText(0, "1")
-        self.scrollArea = QtWidgets.QScrollArea(self.splitter)
+        self.gridLayout_2.addWidget(self.splitter, 0, 0, 1, 1)
+        self.scrollArea = QtWidgets.QScrollArea(self.groupBox)
         self.scrollArea.setWidgetResizable(True)
         self.scrollArea.setObjectName("scrollArea")
-        self.graphicsView = QtWidgets.QGraphicsView()
-        self.graphicsView.setGeometry(QtCore.QRect(0, 0, 330, 582))
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 715, 539))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.gridLayout_3 = QtWidgets.QGridLayout(self.scrollAreaWidgetContents)
+        self.gridLayout_3.setObjectName("gridLayout_3")
+        self.graphicsView = QtWidgets.QGraphicsView(self.scrollAreaWidgetContents)
         self.graphicsView.setObjectName("graphicsView")
-        self.scrollArea.setWidget(self.graphicsView)
-        self.gridLayout_2.addWidget(self.splitter, 0, 0, 1, 1)
+        self.gridLayout_3.addWidget(self.graphicsView, 0, 0, 1, 1)
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.gridLayout_2.addWidget(self.scrollArea, 0, 1, 1, 1)
         self.gridLayout.addWidget(self.groupBox, 3, 0, 1, 1)
 
         self.retranslateUi(Dialog_code_image)
@@ -75,7 +79,6 @@ class Ui_Dialog_code_image(object):
         _translate = QtCore.QCoreApplication.translate
         Dialog_code_image.setWindowTitle(_translate("Dialog_code_image", "View Image"))
         self.pushButton_memo.setText(_translate("Dialog_code_image", "Memo"))
-        self.pushButton_select.setText(_translate("Dialog_code_image", "Select image"))
         self.label_coder.setText(_translate("Dialog_code_image", "Coder:"))
         self.checkBox_show_coders.setToolTip(_translate("Dialog_code_image", "Mark this to show all coded text by all other coders."))
         self.checkBox_show_coders.setText(_translate("Dialog_code_image", "Show other coders"))
@@ -90,4 +93,3 @@ if __name__ == "__main__":
     ui.setupUi(Dialog_code_image)
     Dialog_code_image.show()
     sys.exit(app.exec_())
-
