3751d4c91ceeb3885c042dd5e9ae21608d28c028
Colin Curtain <ccbogel@hotmail.com>
2021-02-17 07:55:48 +1100

    Add files via upload

--- qualcoder/GUI/ui_dialog_report_code_summary.py
+++ qualcoder/GUI/ui_dialog_report_code_summary.py
@@ -10,13 +10,13 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_Dialog_file_summary(object):
-    def setupUi(self, Dialog_file_summary):
-        Dialog_file_summary.setObjectName("Dialog_file_summary")
-        Dialog_file_summary.resize(880, 670)
-        self.gridLayout = QtWidgets.QGridLayout(Dialog_file_summary)
+class Ui_Dialog_code_summary(object):
+    def setupUi(self, Dialog_code_summary):
+        Dialog_code_summary.setObjectName("Dialog_code_summary")
+        Dialog_code_summary.resize(880, 670)
+        self.gridLayout = QtWidgets.QGridLayout(Dialog_code_summary)
         self.gridLayout.setObjectName("gridLayout")
-        self.groupBox_2 = QtWidgets.QGroupBox(Dialog_file_summary)
+        self.groupBox_2 = QtWidgets.QGroupBox(Dialog_code_summary)
         self.groupBox_2.setTitle("")
         self.groupBox_2.setObjectName("groupBox_2")
         self.gridLayout_2 = QtWidgets.QGridLayout(self.groupBox_2)
@@ -39,20 +39,20 @@ class Ui_Dialog_file_summary(object):
         self.gridLayout_2.addWidget(self.splitter, 1, 0, 1, 1)
         self.gridLayout.addWidget(self.groupBox_2, 1, 1, 1, 1)
 
-        self.retranslateUi(Dialog_file_summary)
-        QtCore.QMetaObject.connectSlotsByName(Dialog_file_summary)
+        self.retranslateUi(Dialog_code_summary)
+        QtCore.QMetaObject.connectSlotsByName(Dialog_code_summary)
 
-    def retranslateUi(self, Dialog_file_summary):
+    def retranslateUi(self, Dialog_code_summary):
         _translate = QtCore.QCoreApplication.translate
-        Dialog_file_summary.setWindowTitle(_translate("Dialog_file_summary", "File summary"))
-        self.label_codes.setText(_translate("Dialog_file_summary", "Code summary report"))
+        Dialog_code_summary.setWindowTitle(_translate("Dialog_code_summary", "Code summary"))
+        self.label_codes.setText(_translate("Dialog_code_summary", "Code summary report"))
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    Dialog_file_summary = QtWidgets.QDialog()
-    ui = Ui_Dialog_file_summary()
-    ui.setupUi(Dialog_file_summary)
-    Dialog_file_summary.show()
+    Dialog_code_summary = QtWidgets.QDialog()
+    ui = Ui_Dialog_code_summary()
+    ui.setupUi(Dialog_code_summary)
+    Dialog_code_summary.show()
     sys.exit(app.exec_())
