bd8ba60378aa7caa53e17d97201244e08e9c3d0e
Colin Curtain <ccbogel@hotmail.com>
2021-09-08 07:52:03 +1000

    Add files via upload

---
+++ qualcoder/save_sql_query.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (c) 2021 Colin Curtain
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+Author: Colin Curtain (ccbogel)
+https://github.com/ccbogel/QualCoder
+https://qualcoder.wordpress.com/
+"""
+
+from PyQt5 import QtWidgets, QtCore
+import os
+import sys
+import logging
+import traceback
+
+from .GUI.ui_save_query import Ui_DialogSaveQuery
+
+path = os.path.abspath(os.path.dirname(__file__))
+logger = logging.getLogger(__name__)
+
+def exception_handler(exception_type, value, tb_obj):
+    """ Global exception handler useful in GUIs.
+    tb_obj: exception.__traceback__ """
+    tb = '\n'.join(traceback.format_tb(tb_obj))
+    text = 'Traceback (most recent call last):\n' + tb + '\n' + exception_type.__name__ + ': ' + str(value)
+    print(text)
+    logger.error(_("Uncaught exception: ") + text)
+    QtWidgets.QMessageBox.critical(None, _('Uncaught Exception'), text)
+
+
+class DialogSaveSql(QtWidgets.QDialog):
+    """
+    Obtains query name and group for saving into stored_sql.
+    Called from:
+         report_sql.save_query
+    """
+
+    name = ""
+    grouper = ""
+    description = ""
+
+    def __init__(self, app, parent=None):
+        """ """
+
+        sys.excepthook = exception_handler
+        QtWidgets.QDialog.__init__(self)
+        self.ui = Ui_DialogSaveQuery()
+        self.ui.setupUi(self)
+        self.setWindowFlags(self.windowFlags() & ~QtCore.Qt.WindowContextHelpButtonHint)
+        font = 'font: ' + str(app.settings['fontsize']) + 'pt '
+        font += '"' + app.settings['font'] + '";'
+        self.setStyleSheet(font)
+
+    def accepted(self):
+        """ Accepted button overridden method """
+        
+        self.name = self.ui.lineEdit_name.text()
+        self.grouper = self.ui.lineEdit_group.text()
+        self.description = self.ui.textEdit.toPlainText()
+        self.ui.Dialog_information.accept()
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    ui = DialogInformation(None, "a title", "")
+    ui.show()
+    sys.exit(app.exec_())
