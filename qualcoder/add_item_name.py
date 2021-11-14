b0fecd1a55065224752918b420b49453883bcd5d
Colin Curtain <ccbogel@hotmail.com>
2021-11-14 22:04:31 +1100

    refactoring

--- qualcoder/add_item_name.py
+++ qualcoder/add_item_name.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-'''
-Copyright (c) 2019 Colin Curtain
+"""
+Copyright (c) 2021 Colin Curtain
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -23,19 +23,20 @@ THE SOFTWARE.
 
 Author: Colin Curtain (ccbogel)
 https://github.com/ccbogel/QualCoder
-'''
+"""
 
-from PyQt5 import QtWidgets, QtGui, QtCore
-from PyQt5.QtCore import Qt, pyqtSignal
 import os
 import sys
 import logging
 import traceback
 
+from PyQt5 import QtWidgets, QtCore
+
 from .GUI.ui_dialog_add_item import Ui_Dialog_add_item
 
 path = os.path.abspath(os.path.dirname(__file__))
 logger = logging.getLogger(__name__)
+
 
 def exception_handler(exception_type, value, tb_obj):
     """ Global exception handler useful in GUIs.
@@ -54,21 +55,18 @@ class DialogAddItemName(QtWidgets.QDialog):
     Requires a name for Dialog title (and label in setupUI)
     Requires a list of dictionary 'name' items.
     Dialog returns ok if the item is not a duplicate of a name in the list.
-    Returns one item through getnewItem method.
+    Returns one item through get_new_name method.
     """
 
-    newItem = None
-    existingItems = []
+    new_item = None
+    existing_items = []
     Dialog_addItem = None
-    typeOfItem = ""  # for dialog title and label: Code or Category
 
     def __init__(self, app, items, title, text, parent=None):
-        super(DialogAddItemName, self).__init__(parent)  # overrride accept method
-
+        super(DialogAddItemName, self).__init__(parent)
         sys.excepthook = exception_handler
         for i in items:
-            self.existingItems.append(i['name'])
-
+            self.existing_items.append(i['name'])
         QtWidgets.QDialog.__init__(self)
         self.ui = Ui_Dialog_add_item()
         self.ui.setupUi(self)
@@ -82,95 +80,14 @@ class DialogAddItemName(QtWidgets.QDialog):
         """ On pressing accept button, check there is no duplicate.
         If no duplicate then accept end close the dialog """
 
-        thisItem = str(self.ui.lineEdit.text())
-        duplicate = False
-        if thisItem in self.existingItems:
-            duplicate = True
+        this_item = str(self.ui.lineEdit.text())
+        if this_item in self.existing_items:
             QtWidgets.QMessageBox.warning(None, _("Duplicated"), _("This already exists"))
             return
-        if duplicate is False:
-            self.newItem = thisItem
+        self.new_item = this_item
         self.close()
 
     def get_new_name(self):
-        """ Get the new name """
+        """ Get the new name. """
 
-        return self.newItem
-
-
-class DialogLinkTo(QtWidgets.QDialog):
-    """
-    Dialog to get a new code or code category from user.
-    Also used for Case and File adding attributes.
-    Requires a name for Dialog title (and label in setupUI)
-    Requires a list of dictionary 'name' items.
-    Dialog returns ok if the item is not a duplicate of a name in the list.
-    Returns one item through getnewItem method.
-    """
-
-    def __init__(self, model, linktypes, fromname, parent=None):
-        super(DialogLinkTo, self).__init__(parent)  # overrride accept method
-        self.linktype = None
-        self.linkitem = None
-        self.linktypes = linktypes
-        self.model = model
-
-        self.setupUi()
-        completer = QtWidgets.QCompleter()#[x['name'] for x in model.nativedata])
-        completer.setCompletionMode(QtWidgets.QCompleter.InlineCompletion)
-        completer.setCompletionColumn(0)
-        completer.setCompletionRole(Qt.DisplayRole)
-        completer.setCaseSensitivity(Qt.CaseInsensitive)
-        completer.setModel(model)
-        self.lineEdit.setCompleter(completer)
-
-        self.combo.setModel(linktypes)
-        self.setWindowTitle('Create link to %s'%fromname)
-        self.setSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed);
-
-    def setupUi(self):
-        self.setObjectName("CreateLinkTo")
-        self.resize(400, 142)
-        self.setFixedSize(400, 142)
-        self.buttonBox = QtWidgets.QDialogButtonBox(self)
-        self.buttonBox.setGeometry(QtCore.QRect(170, 90, 201, 32))
-        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
-        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
-        self.buttonBox.setObjectName("buttonBox")
-        self.lineEdit = QtWidgets.QLineEdit(self)
-        self.lineEdit.setGeometry(QtCore.QRect(20, 40, 351, 27))
-        self.lineEdit.setObjectName("lineEdit")
-        self.label = QtWidgets.QLabel("Link to:",self)
-        self.label.setGeometry(QtCore.QRect(20, 20, 141, 17))
-        self.label.setObjectName("label")
-        self.labelc = QtWidgets.QLabel("Linktype:",self)
-        self.labelc.setGeometry(QtCore.QRect(20, 70, 141, 17))
-        self.labelc.setObjectName("linktype_label")
-        self.combo = QtWidgets.QComboBox(self)
-        self.combo.setGeometry(QtCore.QRect(20, 90, 141, 27))
-        self.lineEdit.setObjectName("linktype")
-        self.buttonBox.accepted.connect(self.accept)
-        self.buttonBox.rejected.connect(self.reject)
-        # QtCore.QMetaObject.connectSlotsByName(self)
-        # self.setTabOrder(self.combo,self.lineEdit, self.buttonBox)
-        self.lineEdit.setFocus(True)
-
-    def accept(self):
-        """ On pressing accept button, check there is no duplicate.
-        If no duplicate then accept end close the dialog """
-
-        thisItem = str(self.lineEdit.text())
-        if thisItem in {v['name'] for v in self.model.nativedata.values()}:
-            self.linkitem = thisItem
-            self.linktype = self.linktypes.nativedata[self.combo.currentIndex()]
-            self.close()
-        else:
-            QtWidgets.QMessageBox.warning(None, _("Not existing"), _("This does not exists"))
-
-
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    ui = DialogAddItemName([{"name":"aaa"}, {"name":"bbb"}], "title")
-    ui.show()
-    sys.exit(app.exec_())
-
+        return self.new_item
