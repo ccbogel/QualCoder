52c09cd3949b5405f3d3e333464c9a750e949177
QC Developer <ccbogel@hotmail.com>
2022-03-09 08:26:52 +1100

    added unique constraint to journal names

--- qualcoder/__main__.py
+++ qualcoder/__main__.py
@@ -1399,7 +1399,8 @@ class MainWindow(QtWidgets.QMainWindow):
         cur.execute(
             "CREATE TABLE code_name (cid integer primary key, name text, memo text, catid integer, owner text,"
             "date text, color text, unique(name))")
-        cur.execute("CREATE TABLE journal (jid integer primary key, name text, jentry text, date text, owner text)")
+        cur.execute("CREATE TABLE journal (jid integer primary key, name text, jentry text, date text, owner text), "
+                    "unique(name)")
         cur.execute("CREATE TABLE stored_sql (title text, description text, grouper text, ssql text, unique(title))")
         cur.execute("INSERT INTO project VALUES(?,?,?,?,?,?,?)",
                     ('v5', datetime.datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S"), '', qualcoder_version, 0,
