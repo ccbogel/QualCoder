2a4a628eaa03d7d0274e5e65c65db8bbe041e290
QC Developer <ccbogel@hotmail.com>
2022-03-10 08:18:47 +1100

    Loading more codig data
    Next todo: insert code_image, code_av data
    Not done: source cases, source attributes, source code_categories

--- qualcoder/merge_projects.py
+++ qualcoder/merge_projects.py
@@ -59,12 +59,11 @@ class MergeProjects:
     The merge will combine files, and codings.
     Copies files from source to destination folders.
     Adds new source code names to dest database.
-    Adds text codings, annotations to dest database.
+    Adds text codings, annotations, image codings, av codings to dest database.
 
     The merge will not insert the Source Categories structure into the Destination.
     TODO
         Does not add new category names.
-        Does not add image or A/V codings.
         Does not add journals.
         Does not add cases.
         Does not add attributes.
@@ -81,16 +80,12 @@ class MergeProjects:
     journals_s = []
     stored_sql_s = []
     summary_msg = ""
-
-    # TODO
     code_image_s = []  # coded image areas from Source project
     code_av_s = []  # coded A/V segments from Source project
-
     code_name_s = []  # code names from Source project
-    # TODO to import code cats
     code_cat_s = []  # code cats from Source project
 
-    # TODO import journals, stored_sql, cases, case_text, attributes
+    # TODO import cases, case_text, attribute_types, attributes
 
     def __init__(self, app, path_s):
         self.app = app
@@ -101,7 +96,8 @@ class MergeProjects:
         self.summary_msg = _("Merging: ") + self.path_s + "\n" + _("Into: ") + self.app.project_path + "\n"
         self.copy_source_files_into_destination()
         self.get_source_data()
-        self.fill_sources_update_id()
+        self.fill_sources_get_new_file_ids()
+        self.update_coding_file_ids()
         self.update_code_name_cid()
         self.insert_data_into_destination()
         print("Finished merging ", self.path_s, " into ", self.path_d)
@@ -130,13 +126,18 @@ class MergeProjects:
                 cid = cur_d.fetchone()[0]
                 cn['newcid'] = cid
                 self.summary_msg += _("Adding code name: ") + cn['name'] + "\n"
-                print(_("Adding new code name: " + cn['name'] + ", " + str(cn['newcid'])))
-        # Update code_text cids
+        # Update code_text, code_image, code_av cids
         print("Updating Source.code_text cid to Destination.cid")
         for cn in self.code_name_s:
             for ct in self.code_text_s:
                 if ct['cid'] == cn['cid']:
                     ct['newcid'] = cn['newcid']
+            for ci in self.code_image_s:
+                if ci['cid'] == cn['cid']:
+                    ci['newcid'] = cn['newcid']
+            for cav in self.code_av_s:
+                if cav['cid'] == cn['cid']:
+                    cav['newcid'] = cn['newcid']
 
     def insert_data_into_destination(self):
         """ Code text fid and cid updated, annotation fid updated.
@@ -171,13 +172,13 @@ class MergeProjects:
             cur_d.execute("insert into annotation (fid,pos0,pos1,memo,owner,date) values(?,?,?,?,?,?)",
                           [a["newfid"], a["pos0"], a["pos1"], a["memo"], a["owner"], a["date"]])
         self.conn_d.commit()
-        #TODO
+        #TODO code_image, code_av
 
-    def fill_sources_update_id(self):
+    def fill_sources_get_new_file_ids(self):
         """ Insert Source.source into Destination.source, unless source name is already present.
-        update newfid in source_s and code_text_s. """
+        update newfid in source_s and code_text_s.
+        TODO link transcript to A/V file """
 
-        print("Inserting Source.source into Destination.source")
         cur_d = self.conn_d.cursor()
         for src in self.source_s:
             cur_d.execute("select id from source where name=?", [src['name']])
@@ -191,22 +192,31 @@ class MergeProjects:
                 cur_d.execute("select last_insert_rowid()")
                 id_ = cur_d.fetchone()[0]
                 src['newid'] = id_
+
+    def update_coding_file_ids(self):
+
         # Update code_text and annotations fids
         print("Updating code_text.fid and annotation.fid")
         for src in self.source_s:
-            for ct in self.code_text_s:
-                if ct['fid'] == src['id']:
-                    ct['newfid'] = src['newid']
+            for c_text in self.code_text_s:
+                if c_text['fid'] == src['id']:
+                    c_text['newfid'] = src['newid']
             for an in self.annotations_s:
                 if an['fid'] == src['id']:
                     an['newfid'] = src['newid']
+            for c_img in self.code_image_s:
+                if c_img['fid'] == src['id']:
+                    c_img['newfid'] = src['newid']
+            for c_av in self.code_av_s:
+                if c_av['fid'] == src['id']:
+                    c_av['newfid'] = src['newid']
         # Display potential problems
-        for ct in self.code_text_s:
+        '''for ct in self.code_text_s:
             if ct['newfid'] == -1:
                 print("Code Text. No Match for existing fid ", ct)
         for an in self.annotations_s:
             if an['newfid'] == -1:
-                print("Annotation. No Match for existing fid ", an)
+                print("Annotation. No Match for existing fid ", an)'''
 
     def copy_source_files_into_destination(self):
         """ Copy source files into destination project.
@@ -230,18 +240,10 @@ class MergeProjects:
 
     def get_source_data(self):
         """ Load the source data into memory.
-        DONE
-        code_name (cid integer primary key, name text, memo text, catid integer, owner text,date text, color text, unique(name)
         TODO
         ##code_cat (catid integer primary key, name text, owner text, date text, memo text, supercatid integer, unique(name)
         TODO
-        code_av (avid integer primary key,id integer,pos0 integer, pos1 integer, cid integer, "
-            "memo text, date text, owner text, important
-        TODO
-        code_image (imid integer primary key,id integer,x1 integer, y1 integer, width integer, "
-            "height integer, cid integer, memo text, date text, owner text, important
-        TODO
-        CREATE TABLE cases (caseid integer primary key, name text, memo text, owner text,date text
+        cases (caseid integer primary key, name text, memo text, owner text,date text
         TODO
         case_text (id integer primary key, caseid integer, fid integer, pos0 integer, pos1 integer, "
             "owner text, date text, memo
@@ -256,22 +258,28 @@ class MergeProjects:
         self.journals_s = []
         self.stored_sql_s = []
         self.code_name_s = []
+        self.code_cat_s = []
         self.code_text_s = []
         self.annotations_s = []
+        self.code_image_s = []
+        self.code_av_s = []
         print("Getting Source table data for source, code_text, code_name, annotation")
         cur_s = self.conn_s.cursor()
+        # Journal data
         sql_journal = "select name, jentry, date, owner from journal"
         cur_s.execute(sql_journal)
         res_journals = cur_s.fetchall()
         for i in res_journals:
             src = {"name": i[0], "jentry": i[1], "date": i[2], "owner": i[3]}
             self.journals_s.append(src)
+        # Stored sql data
         sql_stored_sql = "select title, description, grouper, ssql from stored_sql"
         cur_s.execute(sql_stored_sql)
         res_stored_sqls = cur_s.fetchall()
         for i in res_stored_sqls:
             src = {"title": i[0], "description": i[1], "grouper": i[2], "ssql": i[3]}
             self.stored_sql_s.append(src)
+        # Source data
         sql_source = "select id, name, fulltext,mediapath,memo,owner,date from source"
         cur_s.execute(sql_source)
         res_source = cur_s.fetchall()
@@ -279,6 +287,7 @@ class MergeProjects:
             src = {"id": i[0], "newid": -1, "name": i[1], "fulltext": i[2], "mediapath": i[3], "memo": i[4],
                    "owner": i[5], "date": i[6]}
             self.source_s.append(src)
+        # Code data
         sql_codenames = "select cid, name, memo, owner, date, color from code_name"
         cur_s.execute(sql_codenames)
         res_codenames = cur_s.fetchall()
@@ -286,6 +295,15 @@ class MergeProjects:
             cn = {"cid": i[0], "newcid": -1, "name": i[1], "memo": i[2], "owner": i[3], "date": i[4], "color": i[5],
                   "catid": None}
             self.code_name_s.append(cn)
+        # Code category data
+        sql_codecats = "select catid, supercatid, name, memo, owner, date from code_cat"
+        cur_s.execute(sql_codecats)
+        res_codecats = cur_s.fetchall()
+        for i in res_codecats:
+            ccat = {"catid": i[0], "newcatid": -1, "supercatid": i[1], "newsupercatid": -1,
+                    "name": i[2], "memo": i[3], "owner": i[4], "date": i[5]}
+            self.code_cat_s.append(ccat)
+        # Code text and text annotation data
         sql_codetext = "select cid, fid, seltext, pos0, pos1, owner, date, memo, important from code_text"
         cur_s.execute(sql_codetext)
         res_codetext = cur_s.fetchall()
@@ -299,3 +317,19 @@ class MergeProjects:
         for i in res_annot:
             an = {"fid": i[0], "newfid": -1, "pos0": i[1], "pos1": i[2], "memo": i[3], "owner": i[4], "date": i[5]}
             self.annotations_s.append(an)
+        # Code image data
+        sql_code_img = "select cid, id, x1, y1, width, height, memo, date, owner, important from code_image"
+        cur_s.execute(sql_code_img)
+        res_code_img = cur_s.fetchall()
+        for i in res_code_img:
+            cimg = {"cid": i[0], "newcid": -1, "fid": i[1], "newfid": -1, "x1": i[2], "y1": i[3],
+                  "width": i[4], "height": i[5], "memo": i[6], "date": i[7], "owner": i[8], "important": i[9]}
+            self.code_image_s.append(cimg)
+        # Code AV data
+        sql_code_av = "select cid, id, pos0, pos1, owner, date, memo, important from code_av"
+        cur_s.execute(sql_code_av)
+        res_code_av = cur_s.fetchall()
+        for i in res_code_av:
+            c_av = {"cid": i[0], "newcid": -1, "fid": i[1], "newfid": -1, "pos0": i[2], "pos1": i[3],
+                  "owner": i[4], "date": i[5], "memo": i[6], "important": i[7]}
+            self.code_av_s.append(c_av)
