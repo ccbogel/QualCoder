49f9a03bd6f5a239f0589937e50e69334a39efe8
QC Developer <ccbogel@hotmail.com>
2022-04-16 09:27:02 +1000

    work in progress
    some pie and bar charts of codes can be displayed

--- qualcoder/view_charts.py
+++ qualcoder/view_charts.py
@@ -26,6 +26,7 @@ https://github.com/ccbogel/QualCoder
 https://qualcoder.wordpress.com/
 """
 
+import base64
 from collections import Counter
 from copy import deepcopy
 import logging
@@ -71,7 +72,6 @@ class ViewCharts(QDialog):
     app = None
     conn = None
     settings = None
-    scene = None
     categories = []
     code_names = []
     dialog_list = []
@@ -91,34 +91,130 @@ class ViewCharts(QDialog):
         font += '"' + self.app.settings['font'] + '";'
         self.setStyleSheet(font)
         self.setWindowFlags(self.windowFlags() & ~QtCore.Qt.WindowType.WindowContextHelpButtonHint)
-        pm = QtGui.QPixmap()
+        '''pm = QtGui.QPixmap()
         pm.loadFromData(QtCore.QByteArray.fromBase64(doc_export_icon), "png")
         self.ui.pushButton_export.setIcon(QtGui.QIcon(pm))
-        #self.ui.pushButton_export.pressed.connect(self.export_image)
-
-        # Set the scene
-        self.scene = GraphicsScene(self)
-        self.ui.graphicsView.setScene(self.scene)
-        self.ui.graphicsView.setContextMenuPolicy(QtCore.Qt.ContextMenuPolicy.DefaultContextMenu)
+        self.ui.pushButton_export.pressed.connect(self.export_image)'''
 
         self.code_names, self.categories = app.get_codes_categories()
-
-        self.ui.comboBox_chart_types.currentIndexChanged.connect(self.show_chart)
-        combobox_list = ['', 'Pie - code frequency',
-                         'Pie - code volume by characters',
-                         'Pie - code volume by image area',
-                         'Pie - code volume by audio/video segments'
+        self.ui.comboBox_pie_charts.currentIndexChanged.connect(self.show_pie_chart)
+        pie_combobox_list = ['', 'Code frequency',
+                         'Code by characters',
+                         'Code by image area',
+                         'Code by audio/video segments'
                          ]
-        self.ui.comboBox_chart_types.addItems(combobox_list)
+        self.ui.comboBox_pie_charts.addItems(pie_combobox_list)
+        self.ui.comboBox_bar_charts.currentIndexChanged.connect(self.show_bar_chart)
+        bar_combobox_list = ['', 'Code frequency',
+                         'Code by characters',
+                         'Code by image area',
+                         'Code by audio/video segments'
+                         ]
+        self.ui.comboBox_bar_charts.addItems(bar_combobox_list)
 
-    def show_chart(self):
+
+    def show_bar_chart(self):
+        """ https://www.tutorialspoint.com/plotly/plotly_bar_and_pie_chart.htm
+        """
+
+        chart_type = self.ui.comboBox_bar_charts.currentText()
+        if chart_type == "":
+            return
+        if chart_type == "Code frequency":
+            self.barchart_code_frequency()
+        if chart_type == "Code by characters":
+            self.barchart_code_volume_by_characters()
+        if chart_type == "Code by image area":
+            self.barchart_code_volume_by_area()
+        if chart_type == "Code by audio/video segments":
+            self.barchart_code_volume_by_segments()
+
+    def barchart_code_frequency(self):
+        """ Count of codes across text, images and A/V.
+        """
+
+        cur = self.app.conn.cursor()
+        values = []
+        labels = []
+        for c in self.code_names:
+            sql = "select count(cid) from code_text where cid=?"
+            cur.execute(sql, [c['cid']])
+            res_text = cur.fetchone()
+            sql = "select count(cid) from code_image where cid=?"
+            cur.execute(sql, [c['cid']])
+            res_image = cur.fetchone()
+            sql = "select count(cid) from code_av where cid=?"
+            cur.execute(sql, [c['cid']])
+            res_av = cur.fetchone()
+            labels.append(c['name'])
+            values.append(res_text[0] + res_image[0] + res_av[0])
+        # Create pandas DataFrame
+        data = {'Code names': labels, 'Count': values}
+        df = pd.DataFrame(data)
+        fig = px.bar(df, x='Code names', y='Count', title='Code count - text, images and Audio/Video')
+        fig.show()
+
+    def barchart_code_volume_by_characters(self):
+        """ Count of codes in files text by character volume. """
+
+        cur = self.app.conn.cursor()
+        values = []
+        labels = []
+        for c in self.code_names:
+            sql = "select sum(pos1 - pos0) from code_text where cid=?"
+            cur.execute(sql, [c['cid']])
+            res_text = cur.fetchone()
+            labels.append(c['name'])
+            values.append(res_text[0])
+        # Create pandas DataFrame
+        data = {'Code names': labels, 'Character count': values}
+        df = pd.DataFrame(data)
+        fig = px.bar(df, x='Code names', y='Character count', title='Code text by character count')
+        fig.show()
+
+    def barchart_code_volume_by_area(self):
+        """ Codes by image area volume. """
+
+        cur = self.app.conn.cursor()
+        values = []
+        labels = []
+        for c in self.code_names:
+            sql = "select sum(cast(width as int) * cast(height as int)) from code_image where cid=?"
+            cur.execute(sql, [c['cid']])
+            res_text = cur.fetchone()
+            labels.append(c['name'])
+            values.append(res_text[0])
+        # Create pandas DataFrame
+        data = {'Code names': labels, 'Pixels': values}
+        df = pd.DataFrame(data)
+        fig = px.bar(df, x='Code names', y='Pixels', title='Code volume by image area (pixels)')
+        fig.show()
+
+    def barchart_code_volume_by_segments(self):
+        """ Codes by audio/video segment volume. """
+
+        cur = self.app.conn.cursor()
+        values = []
+        labels = []
+        for c in self.code_names:
+            sql = "select sum(pos1 - pos0) from code_av where cid=?"
+            cur.execute(sql, [c['cid']])
+            res_text = cur.fetchone()
+            labels.append(c['name'])
+            values.append(res_text[0])
+        # Create pandas DataFrame
+        data = {'Code names': labels, 'Segments': values}
+        df = pd.DataFrame(data)
+        fig = px.bar(df, x='Code names', y='Segments', title='Code volume by audio/video segments (milliseconds)')
+        fig.show()
+
+    def show_pie_chart(self):
         """
         https://plotly.com/python/pie-charts/
-
         https://www.tutorialspoint.com/plotly/plotly_bar_and_pie_chart.htm
         """
 
-        chart_type = self.ui.comboBox_chart_types.currentText()
+        chart_type = self.ui.comboBox_pie_charts.currentText()
         if chart_type == "":
             return
         '''
@@ -129,17 +225,19 @@ class ViewCharts(QDialog):
         fig = px.pie(df, values='pop', names='country', title='Population of European continent')
         fig.show()'''
 
-        if chart_type == "Pie - code frequency":
+        if chart_type == "Code frequency":
             self.piechart_code_frequency()
-        if chart_type == "Pie - code volume by characters":
+        if chart_type == "Code by characters":
             self.piechart_code_volume_by_characters()
-        if chart_type == "Pie - code volume by image area":
+        if chart_type == "Code by image area":
             self.piechart_code_volume_by_area()
-        if chart_type == "Pie - code volume by audio/video segments":
+        if chart_type == "Code by audio/video segments":
             self.piechart_code_volume_by_segments()
 
     def piechart_code_frequency(self):
-        """ Count of codes across text, images and A/V. """
+        """ Count of codes across text, images and A/V.
+        can render differntly: fig.show(renderer="svg")
+        """
 
         cur = self.app.conn.cursor()
         values = []
@@ -157,6 +255,12 @@ class ViewCharts(QDialog):
             labels.append(c['name'])
             values.append(res_text[0] + res_image[0] + res_av[0])
         fig = px.pie(values=values, names=labels, title='Code count - text, images and Audio/Video')
+        '''img_bytes = fig.to_image(format="png") # pip install kaledio
+        encoding = base64.b64encode(img_bytes).decode()
+        img_b64 = "data:image/png;base64," + encoding
+        pm = QtGui.QPixmap()
+        pm.loadFromData(QtCore.QByteArray.fromBase64(encoding), "png")
+        self.ui.label_bar_chart.setPixmap(pm)'''
         fig.show()
 
     def piechart_code_volume_by_characters(self):
@@ -204,39 +308,3 @@ class ViewCharts(QDialog):
         #df = pd.DataFrame(dict(a=[1, 3, 2], b=[3, 2, 1]))
         fig = px.pie(values=values, names=labels, title='Code volume by audio/video segments (milliseconds)')
         fig.show()
-
-
-
-class GraphicsScene(QtWidgets.QGraphicsScene):
-    """ set the scene for the graphics objects and re-draw events. """
-
-    scene_width = 990
-    scene_height = 650
-    parent = None
-
-    def __init__(self, parent=None):
-        super(GraphicsScene, self).__init__(parent)
-        self.parent = parent
-        self.setSceneRect(QtCore.QRectF(0, 0, self.scene_width, self.scene_height))
-
-    def set_width(self, width):
-        """ Resize scene width. """
-
-        self.scene_width = width
-        self.setSceneRect(QtCore.QRectF(0, 0, self.scene_width, self.scene_height))
-
-    def set_height(self, height):
-        """ Resize scene height. """
-
-        self.scene_height = height
-        self.setSceneRect(QtCore.QRectF(0, 0, self.scene_width, self.scene_height))
-
-    def get_width(self):
-        """ Return scene width. """
-
-        return self.scene_width
-
-    def get_height(self):
-        """ Return scene height. """
-
-        return self.scene_height
