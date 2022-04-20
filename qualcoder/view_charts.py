ac502ecb52d243008f33c64377dd37613a963689
QC Developer <ccbogel@hotmail.com>
2022-04-20 22:19:50 +1000

    added value filter

--- qualcoder/view_charts.py
+++ qualcoder/view_charts.py
@@ -34,12 +34,12 @@ import plotly.express as px
 import sys
 import traceback
 
-from PyQt6 import QtCore, QtWidgets
+from PyQt6 import QtCore, QtGui, QtWidgets
 from PyQt6.QtWidgets import QDialog
 
-#from .GUI.base64_helper import *
 from .GUI.ui_dialog_charts import Ui_DialogCharts
-#from .helpers import Message
+
+# from .helpers import Message
 
 path = os.path.abspath(os.path.dirname(__file__))
 logger = logging.getLogger(__name__)
@@ -81,22 +81,39 @@ class ViewCharts(QDialog):
         # Set up the user interface from Designer.
         self.ui = Ui_DialogCharts()
         self.ui.setupUi(self)
+        integers = QtGui.QIntValidator()
+        self.ui.lineEdit_filter.setValidator(integers)
 
         # Temporary hide widgets
         self.ui.label_tree.hide()
         self.ui.comboBox_tree_charts.hide()
-        self.ui.label_coder.hide()
-        self.ui.comboBox_coders.hide()
         self.ui.label_select_case.hide()
         self.ui.comboBox_case.hide()
         self.ui.label_select_file.hide()
         self.ui.comboBox_file.hide()
         self.ui.pushButton_attributes.hide()
+        # Very temp:
+        self.ui.label_coder.hide()
+        self.ui.comboBox_coders.hide()
 
         font = 'font: ' + str(self.app.settings['fontsize']) + 'pt '
         font += '"' + self.app.settings['font'] + '";'
         self.setStyleSheet(font)
         self.setWindowFlags(self.windowFlags() & ~QtCore.Qt.WindowType.WindowContextHelpButtonHint)
+
+        # Get coder names from all tables
+        sql = "select owner from  code_image union select owner from code_text union select owner from code_av "
+        sql += " union select owner from cases union select owner from journal union select owner from attribute "
+        sql += "union select owner from source union select owner from annotation union select owner from code_name "
+        sql += "union select owner from code_cat"
+        coders = [""]
+        cur = self.app.conn.cursor()
+        cur.execute(sql)
+        results = cur.fetchall()
+        for row in results:
+            if row[0] != "":
+                coders.append(row[0])
+        self.ui.comboBox_coders.addItems(coders)
         self.code_names, self.categories = app.get_codes_categories()
         self.ui.comboBox_pie_charts.currentIndexChanged.connect(self.show_pie_chart)
         pie_combobox_list = ['', _('Code frequency'),
@@ -107,14 +124,14 @@ class ViewCharts(QDialog):
         self.ui.comboBox_pie_charts.addItems(pie_combobox_list)
         self.ui.comboBox_sunburst_charts.currentIndexChanged.connect(self.show_hierarchy_chart)
         sunburst_combobox_list = ['', _('Code frequency sunburst'),
-                             _('Code frequency treemap'),
-                             _('Code by characters sunburst'),
-                             _('Code by characters treemap'),
-                             _('Code by image area sunburst'),
-                             _('Code by image area treemap'),
-                             _('Code by A/V sunburst'),
-                             _('Code by A/V treemap')
-                             ]
+                                  _('Code frequency treemap'),
+                                  _('Code by characters sunburst'),
+                                  _('Code by characters treemap'),
+                                  _('Code by image area sunburst'),
+                                  _('Code by image area treemap'),
+                                  _('Code by A/V sunburst'),
+                                  _('Code by A/V treemap')
+                                  ]
         self.ui.comboBox_sunburst_charts.addItems(sunburst_combobox_list)
         self.ui.comboBox_bar_charts.currentIndexChanged.connect(self.show_bar_chart)
         bar_combobox_list = ['', _('Code frequency'),
@@ -160,12 +177,16 @@ class ViewCharts(QDialog):
             res_av = cur.fetchone()
             labels.append(c['name'])
             values.append(res_text[0] + res_image[0] + res_av[0])
+        title = _('Code count - text, images and Audio/Video')
         # Create pandas DataFrame
         data = {'Code names': labels, 'Count': values}
         df = pd.DataFrame(data)
+        cutoff = self.ui.lineEdit_filter.text()
         mask = df['Count'] != 0
-        fig = px.bar(df[mask], y='Code names', x='Count', orientation='h',
-                     title=_('Code count - text, images and Audio/Video'))
+        if cutoff != "":
+            mask = df['Count'] >= int(cutoff)
+            title += "<br><sup>" + _("Values") + " >= " + cutoff + "<sup>"
+        fig = px.bar(df[mask], y='Code names', x='Count', orientation='h', title=title)
         fig.show()
 
     def barchart_code_volume_by_characters(self):
@@ -177,15 +198,19 @@ class ViewCharts(QDialog):
         for c in self.code_names:
             sql = "select sum(pos1 - pos0) from code_text where cid=?"
             cur.execute(sql, [c['cid']])
-            res_text = cur.fetchone()
+            res = cur.fetchone()
             labels.append(c['name'])
-            values.append(res_text[0])
+            values.append(res[0])
+        title = _('Code text by character count')
         # Create pandas DataFrame
-        data = {'Code names': labels, 'Character count': values}
+        data = {'Code names': labels, 'Total characters': values}
         df = pd.DataFrame(data)
-        mask = df['Character count'] != 0
-        fig = px.bar(df[mask], y='Code names', x='Character count', orientation='h',
-                     title=_('Code text by character count'))
+        mask = df['Total characters'] != 0
+        cutoff = self.ui.lineEdit_filter.text()
+        if cutoff != "":
+            mask = df['Total characters'] >= int(cutoff)
+            title += "<br><sup>" + _("Values") + " >= " + cutoff + "<sup>"
+        fig = px.bar(df[mask], x='Total characters', y='Code names', orientation='h', title=title)
         fig.show()
 
     def barchart_code_volume_by_area(self):
@@ -197,15 +222,19 @@ class ViewCharts(QDialog):
         for c in self.code_names:
             sql = "select sum(cast(width as int) * cast(height as int)) from code_image where cid=?"
             cur.execute(sql, [c['cid']])
-            res_text = cur.fetchone()
+            res = cur.fetchone()
             labels.append(c['name'])
-            values.append(res_text[0])
+            values.append(res[0])
+        title = _('Code volume by image area (pixels)')
         # Create pandas DataFrame
         data = {'Code names': labels, 'Pixels': values}
         df = pd.DataFrame(data)
         mask = df['Pixels'] != 0
-        fig = px.bar(df[mask], y='Code names', x='Pixels', orientation='h',
-                     title=_('Code volume by image area (pixels)'))
+        cutoff = self.ui.lineEdit_filter.text()
+        if cutoff != "":
+            mask = df['Pixels'] >= int(cutoff)
+            title += "<br><sup>" + _("Values") + " >= " + cutoff + "<sup>"
+        fig = px.bar(df[mask], x='Pixels', y='Code names', orientation='h', title=title)
         fig.show()
 
     def barchart_code_volume_by_segments(self):
@@ -217,15 +246,19 @@ class ViewCharts(QDialog):
         for c in self.code_names:
             sql = "select sum(pos1 - pos0) from code_av where cid=?"
             cur.execute(sql, [c['cid']])
-            res_text = cur.fetchone()
+            res = cur.fetchone()
             labels.append(c['name'])
-            values.append(res_text[0])
+            values.append(res[0])
+        title = _('Code volume by audio/video segments (milliseconds)')
         # Create pandas DataFrame
-        data = {'Code names': labels, 'Segments': values}
+        data = {'Code names': labels, 'Total millisecs': values}
         df = pd.DataFrame(data)
-        mask = df['Segments'] != 0
-        fig = px.bar(df[mask], y='Code names', x='Segments', orientation='h'
-                     , title=_('Code volume by audio/video segments (milliseconds)'))
+        mask = df['Total millisecs'] != 0
+        cutoff = self.ui.lineEdit_filter.text()
+        if cutoff != "":
+            mask = df['Total millisecs'] >= int(cutoff)
+            title += "<br><sup>" + _("Values") + " >= " + cutoff + "<sup>"
+        fig = px.bar(df[mask], x='Total millisecs', y='Code names', orientation='h', title=title)
         fig.show()
 
     def show_pie_chart(self):
@@ -267,12 +300,16 @@ class ViewCharts(QDialog):
             res_av = cur.fetchone()
             labels.append(c['name'])
             values.append(res_text[0] + res_image[0] + res_av[0])
+        title = _('Code count - text, images and Audio/Video')
         # Create pandas DataFrame
-        data = {'Code names': labels, 'Character count': values}
+        data = {'Code names': labels, 'Count': values}
         df = pd.DataFrame(data)
-        mask = df['Character count'] != 0
-        fig = px.pie(df[mask], values='Character count', names='Code names',
-                     title=_('Code count - text, images and Audio/Video'))
+        mask = df['Count'] != 0
+        cutoff = self.ui.lineEdit_filter.text()
+        if cutoff != "":
+            mask = df['Count'] >= int(cutoff)
+            title += "<br><sup>" + _("Values") + " >= " + cutoff + "<sup>"
+        fig = px.pie(df[mask], values='Count', names='Code names', title=title)
         fig.show()
 
     def piechart_code_volume_by_characters(self):
@@ -284,10 +321,19 @@ class ViewCharts(QDialog):
         for c in self.code_names:
             sql = "select sum(pos1 - pos0) from code_text where cid=?"
             cur.execute(sql, [c['cid']])
-            res_text = cur.fetchone()
+            res = cur.fetchone()
             labels.append(c['name'])
-            values.append(res_text[0])
-        fig = px.pie(values=values, names=labels, title=_('Code text by character count'))
+            values.append(res[0])
+        title = _('Code text by character count')
+        # Create pandas DataFrame
+        data = {'Code names': labels, 'Total characters': values}
+        df = pd.DataFrame(data)
+        cutoff = self.ui.lineEdit_filter.text()
+        mask = df['Total characters'] != 0
+        if cutoff != "":
+            mask = df['Total characters'] >= int(cutoff)
+            title += "<br><sup>" + _("Values") + " >= " + cutoff + "<sup>"
+        fig = px.pie(df[mask], values='Total characters', names='Code names', title=title)
         fig.show()
 
     def piechart_code_volume_by_area(self):
@@ -299,10 +345,19 @@ class ViewCharts(QDialog):
         for c in self.code_names:
             sql = "select sum(cast(width as int) * cast(height as int)) from code_image where cid=?"
             cur.execute(sql, [c['cid']])
-            res_text = cur.fetchone()
+            res = cur.fetchone()
             labels.append(c['name'])
-            values.append(res_text[0])
-        fig = px.pie(values=values, names=labels, title=_('Code volume by image area (pixels)'))
+            values.append(res[0])
+        title = _('Code volume by image area (pixels)')
+        # Create pandas DataFrame
+        data = {'Code names': labels, 'Total pixels': values}
+        df = pd.DataFrame(data)
+        cutoff = self.ui.lineEdit_filter.text()
+        mask = df['Total pixels'] != 0
+        if cutoff != "":
+            mask = df['Total pixels'] >= int(cutoff)
+            title += "<br><sup>" + _("Values") + " >= " + cutoff + "<sup>"
+        fig = px.pie(df[mask], values='Total pixels', names='Code names', title=title)
         fig.show()
 
     def piechart_code_volume_by_segments(self):
@@ -314,12 +369,19 @@ class ViewCharts(QDialog):
         for c in self.code_names:
             sql = "select sum(pos1 - pos0) from code_av where cid=?"
             cur.execute(sql, [c['cid']])
-            res_text = cur.fetchone()
+            res = cur.fetchone()
             labels.append(c['name'])
-            values.append(res_text[0])
-        # df = pd.DataFrame(dict(a=[1, 3, 2], b=[3, 2, 1]))
-        fig = px.pie(values=values, names=labels,
-                     title=_('Code volume by audio/video segments (milliseconds)'))
+            values.append(res[0])
+        title = _('Code volume by audio/video segments (milliseconds)')
+        # Create pandas DataFrame
+        data = {'Code names': labels, 'Total millisecs': values}
+        df = pd.DataFrame(data)
+        cutoff = self.ui.lineEdit_filter.text()
+        mask = df['Total millisecs'] != 0
+        if cutoff != "":
+            mask = df['Total millisecs'] >= int(cutoff)
+            title += "<br><sup>" + _("Values") + " >= " + cutoff + "<sup>"
+        fig = px.pie(df[mask], values='Total millisecs', names='Code names', title=title)
         fig.show()
 
     def show_hierarchy_chart(self):
@@ -431,14 +493,23 @@ class ViewCharts(QDialog):
             items.append(sb_combined['name'])
             values.append(sb_combined['count'])
             parents.append(sb_combined['parentname'])
-        data = dict(item=items, value=values, parent=parents)
+        # data = dict(item=items, value=values, parent=parents) # OLDER
+        # Create pandas DataFrame
+        data = {'item': items, 'value': values, 'parent': parents}
+        df = pd.DataFrame(data)
+        cutoff = self.ui.lineEdit_filter.text()
+        mask = df['value'] != 0
+        subtitle = ""
+        if cutoff != "":
+            mask = df['value'] >= int(cutoff)
+            subtitle += "<br><sup>" + _("Values") + " >= " + cutoff + "<sup>"
         if chart == "sunburst":
-            fig = px.sunburst(data, names='item', parents='parent', values='value',
-                title=_('Sunburst chart of counts of codes and categories'))
+            fig = px.sunburst(df[mask], names='item', parents='parent', values='value',
+                              title=_('Sunburst chart of counts of codes and categories') + subtitle)
             fig.show()
         if chart == "treemap":
-            fig = px.treemap(data, names='item', parents='parent', values='value',
-                title=_('Treemap of counts of codes and categories'))
+            fig = px.treemap(df[mask], names='item', parents='parent', values='value',
+                             title=_('Treemap of counts of codes and categories') + subtitle)
             fig.show()
 
     def hierarchy_code_volume_by_characters(self, chart="sunburst"):
@@ -514,14 +585,23 @@ class ViewCharts(QDialog):
             items.append(sb_combined['name'])
             values.append(sb_combined['count'])
             parents.append(sb_combined['parentname'])
-        data = dict(item=items, value=values, parent=parents)
+        #data = dict(item=items, value=values, parent=parents)  # OLDER
+        # Create pandas DataFrame
+        data = {'item': items, 'value': values, 'parent': parents}
+        df = pd.DataFrame(data)
+        cutoff = self.ui.lineEdit_filter.text()
+        mask = df['value'] != 0
+        subtitle = ""
+        if cutoff != "":
+            mask = df['value'] >= int(cutoff)
+            subtitle += "<br><sup>" + _("Values") + " >= " + cutoff + "<sup>"
         if chart == "sunburst":
-            fig = px.sunburst(data, names='item', parents='parent', values='value',
-                title=_('Sunburst chart of coded text - characters'))
+            fig = px.sunburst(df[mask], names='item', parents='parent', values='value',
+                              title=_('Sunburst chart of coded text - characters') + subtitle)
             fig.show()
         if chart == "treemap":
-            fig = px.treemap(data, names='item', parents='parent', values='value',
-                title=_('Treemap of coded text - characters'))
+            fig = px.treemap(df[mask], names='item', parents='parent', values='value',
+                             title=_('Treemap of coded text - characters') + subtitle)
             fig.show()
 
     def hierarchy_code_volume_by_area(self, chart="sunburst"):
@@ -597,14 +677,23 @@ class ViewCharts(QDialog):
             items.append(sb_combined['name'])
             values.append(sb_combined['count'])
             parents.append(sb_combined['parentname'])
-        data = dict(item=items, value=values, parent=parents)
+        #data = dict(item=items, value=values, parent=parents)# OLDER
+        # Create pandas DataFrame
+        data = {'item': items, 'value': values, 'parent': parents}
+        df = pd.DataFrame(data)
+        cutoff = self.ui.lineEdit_filter.text()
+        mask = df['value'] != 0
+        subtitle = ""
+        if cutoff != "":
+            mask = df['value'] >= int(cutoff)
+            subtitle += "<br><sup>" + _("Values") + " >= " + cutoff + "<sup>"
         if chart == "sunburst":
-            fig = px.sunburst(data, names='item', parents='parent', values='value',
-                title=_('Sunburst chart of coded image areas - pixels'))
+            fig = px.sunburst(df[mask], names='item', parents='parent', values='value',
+                              title=_('Sunburst chart of coded image areas - pixels') + subtitle)
             fig.show()
         if chart == "treemap":
-            fig = px.treemap(data, names='item', parents='parent', values='value',
-                title=_('Treemap of coded image areas - pixels'))
+            fig = px.treemap(df[mask], names='item', parents='parent', values='value',
+                             title=_('Treemap of coded image areas - pixels') + subtitle)
             fig.show()
 
     def hierarchy_code_volume_by_segments(self, chart="sunburst"):
@@ -680,15 +769,21 @@ class ViewCharts(QDialog):
             items.append(sb_combined['name'])
             values.append(sb_combined['count'])
             parents.append(sb_combined['parentname'])
-        data = dict(item=items, value=values, parent=parents)
+        #data = dict(item=items, value=values, parent=parents)  # older
+        # Create pandas DataFrame
+        data = {'item': items, 'value': values, 'parent': parents}
+        df = pd.DataFrame(data)
+        cutoff = self.ui.lineEdit_filter.text()
+        mask = df['value'] != 0
+        subtitle = ""
+        if cutoff != "":
+            mask = df['value'] >= int(cutoff)
+            subtitle += "<br><sup>" + _("Values") + " >= " + cutoff + "<sup>"
         if chart == "sunburst":
-            fig = px.sunburst(data, names='item', parents='parent', values='value',
-                title=_('Sunburst chart of coded audio/video segments - milliseconds'))
+            fig = px.sunburst(df[mask], names='item', parents='parent', values='value',
+                              title=_('Sunburst chart of coded audio/video segments - milliseconds') + subtitle)
             fig.show()
         if chart == "treemap":
-            fig = px.treemap(data, names='item', parents='parent', values='value',
-                title=_('Treemap of coded image audio/video segments - milliseconds'))
+            fig = px.treemap(df[mask], names='item', parents='parent', values='value',
+                             title=_('Treemap of coded image audio/video segments - milliseconds') + subtitle)
             fig.show()
-
-
-
