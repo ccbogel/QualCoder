e5ffdf02edb480ed51fc163afd745fc07d05d788
QualCoder Developer <ccbogel@hotmail.com>
2023-02-05 23:31:14 +1100

    Add files via upload

--- qualcoder/color_selector.py
+++ qualcoder/color_selector.py
@@ -82,6 +82,68 @@ colors = [
     "#CEDAEC", "#9EB5D9", "#6D91C6", "#3D6CB3", "#0D47A1", "#E8E8E8", "#D8D8D8", "#C8C8C8", "#B8B8B8", "#A8A8A8"
     ]
 
+# www.color-blindness.com/coblis-color-blindness-simulator/
+# https://imagecolorpicker.com/en
+colors_red_weak = [
+    "#FBF4D0", "#FAF2B3", "#FBF598", "#1E1D03", "#232105", "#F6EDE0", "#F2E4CF", "#EAD4AB", "#E5C584", "#E0B75B",
+    "#F6ECCE", "#F3E2A9", "#F2DB81", "#F1D559", "#F1D02F", "#EFE3E2", "#E6D3D1", "#D5B5B0", "#C7978C", "#BD7C66",
+    "#F3E8E1", "#EBDBD0", "#DFC4AD", "#D5AD87", "#CD9860", "#F0DFCE", "#DFC19D", "#CFA46B", "#C08939", "#B2710F",
+    "#F6E5CD", "#EBCC9C", "#E0B56B", "#D69F38", "#CC8B0F", "#E4D5D3", "#C8AEA9", "#AD877E", "#966454", "#854626",
+    "#E8D9D0", "#D0B6A1", "#B89373", "#A37343", "#905615", "#DCD2DD", "#B7A6BD", "#937C9E", "#705481", "#563264",
+    "#EEE3E8", "#E5D4DB", "#D3B6C3", "#C398AC", "#B77D96", "#ECE4F9", "#E1D5F9", "#CAB8FA", "#B69CFB", "#A582FB",
+    "#DAD6F7", "#B2AFF0", "#8489EC", "#3764E5", "#2D45BF", "#E6E2F9", "#D6D2F9", "#B4B2FB", "#8F90FC", "#5F71FD",
+    "#DBDBD0", "#B5B9A2", "#8F9775", "#697648", "#44561E", "#E6E7E3", "#CCCFC7", "#B1B8AB", "#96A190", "#7C8A75",
+    "#E6EFDF", "#D5EACA", "#C6E8B7", "#B7E7A4", "#9AE977", "#F1F3DD", "#E8EFCA", "#D8E9A4", "#C9E67B", "#BBE652",
+    "#EBEDA6", "#E6EC7D", "#E0EC54", "#DAED36", "#D0EC00", "#E4F0F1", "#D2EBEC", "#C1E9EB", "#B1EAEA", "#89DFE5",
+    "#D9DAF5", "#AFB7ED", "#8393E6", "#4C71E0", "#1951C0", "#D8E0F4", "#BACCF2", "#9AB9F3", "#6091D6", "#4F83FB",
+    "#D4D8EB", "#A6B3D7", "#798EC4", "#486AB1", "#05489D", "#EBE7E8", "#DAD7D8", "#CAC7C8", "#BAB7B8", "#AAA7A8"
+    ]
+
+colors_red_blind = [
+    "#FFF3D0", "#FFF0B9", "#FFF1BC", "#FFF4CC", "#F9DD00", "#F5EDE0", "#EFE5CF", "#E4D6AC", "#DAC985", "#D1BD5D",
+    "#F7EBCE", "#F3E2A9", "#F0DC82", "#EED659", "#EBD22F", "#EAE4E3", "#DCD7D3", "#C3BCB3", "#ACA492", "#9B906E",
+    "#F0E9E1", "#E5DDD1", "#D3C9AF", "#C2B58B", "#B3A464", "#EAE1CF", "#D2C69F", "#BCAD6F", "#A7963D", "#948418",
+    "#F1E7CE", "#F1E7CE", "#CEBC6D", "#BEAA3B", "#AF9C18", "#DDD7D5", "#B9B3AC", "#979183", "#7B735A", "#695F2C",
+    "#E3DBD1", "#C4BAA3", "#A69B76", "#8C7F47", "#766919", "#D6D4DE", "#A9ABC0", "#7E84A4", "#55618A", "#394770",
+    "#E9E5E9", "#DBD7DD", "#BFBDC8", "#A5A5B4", "#9192A1", "#E6E6FB", "#D4D9FC", "#B2C1FF", "#91ABFF", "#749AFF",
+    "#D4D8F8", "#A4B4F4", "#6B90F3", "#006FED", "#0057B7", "#E2E3FA", "#CFD4FA", "#A3B6FF", "#7399FF", "#337FFF",
+    "#E1D9CF", "#BFB6A1", "#9D9373", "#7C7146", "#5B511D", "#EBE5E2", "#D3CDC6", "#BCB5AA", "#A49D8E", "#8D8673",
+    "#F4EBDD", "#EFE3C7", "#EDDFB2", "#EDDC9F", "#F2DC73", "#FAF0DC", "#F7EAC8", "#F3E2A1", "#F2DC78", "#F3DA4F",
+    "#FBE8A4", "#FCE57B", "#FEE452", "#FFE43A", "#FDE100", "#F1ECEF", "#EAE5E9", "#E6E2E6", "#E4E0E5", "#D7D5DF",
+    "#D8DAF5", "#ACB8EE", "#7B95E7", "#3A74E3", "#0057B7", "#DEDEF3", "#C3C9F1", "#A8B6F1", "#798ED2", "#4A84FC",
+    "#D7D7EA", "#ABB2D7", "#7F8DC3", "#4F69B0", "#00499B", "#ECE7E7", "#DCD7D7", "#CBC7C7", "#BBB7B7", "#ABA7A8"
+    ]
+
+colors_green_weak = [
+    "#", "#", "#", "#", "#", "#", "#", "#", "#", "#",
+    "#", "#", "#", "#", "#", "#", "#", "#", "#", "#",
+    "#", "#", "#", "#", "#", "#", "#", "#", "#", "#",
+    "#", "#", "#", "#", "#", "#", "#", "#", "#", "#",
+    "#", "#", "#", "#", "#", "#", "#", "#", "#", "#",
+    "#", "#", "#", "#", "#", "#", "#", "#", "#", "#",
+    "#", "#", "#", "#", "#", "#", "#", "#", "#", "#",
+    "#", "#", "#", "#", "#", "#", "#", "#", "#", "#",
+    "#", "#", "#", "#", "#", "#", "#", "#", "#", "#",
+    "#", "#", "#", "#", "#", "#", "#", "#", "#", "#",
+    "#", "#", "#", "#", "#", "#", "#", "#", "#", "#",
+    "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"
+    ]
+
+colors_green_blind = [
+    "#", "#", "#", "#", "#", "#", "#", "#", "#", "#",
+    "#", "#", "#", "#", "#", "#", "#", "#", "#", "#",
+    "#", "#", "#", "#", "#", "#", "#", "#", "#", "#",
+    "#", "#", "#", "#", "#", "#", "#", "#", "#", "#",
+    "#", "#", "#", "#", "#", "#", "#", "#", "#", "#",
+    "#", "#", "#", "#", "#", "#", "#", "#", "#", "#",
+    "#", "#", "#", "#", "#", "#", "#", "#", "#", "#",
+    "#", "#", "#", "#", "#", "#", "#", "#", "#", "#",
+    "#", "#", "#", "#", "#", "#", "#", "#", "#", "#",
+    "#", "#", "#", "#", "#", "#", "#", "#", "#", "#",
+    "#", "#", "#", "#", "#", "#", "#", "#", "#", "#",
+    "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"
+    ]
+
 COLS = 10
 ROWS = 12
 
@@ -125,7 +187,7 @@ class DialogColorSelect(QtWidgets.QDialog):
         cur = app.conn.cursor()
         cur.execute("select color, name from code_name order by name")
         self.used_colors = cur.fetchall()
-        self.setup_ui()
+        self.fill_table()
         self.setWindowFlags(self.windowFlags() & ~QtCore.Qt.WindowType.WindowContextHelpButtonHint)
         font = 'font: ' + str(app.settings['fontsize']) + 'pt '
         font += '"' + app.settings['font'] + '";'
@@ -140,6 +202,35 @@ class DialogColorSelect(QtWidgets.QDialog):
         self.ui.label_colour_old.setText(code_['name'])
         self.ui.label_colour_new.setToolTip(_("New colour"))
         self.ui.label_colour_new.setText(code_['name'])
+
+    def event(self, event):
+        """ Using this event filter to apply appearance of several types of colour blindness
+        N normal vision
+        R Red weak
+        Shift R Red blind
+        G Green Weak
+        Shift G Green blind
+        """
+
+        if type(event) == QtGui.QKeyEvent:
+            key = event.key()
+            mod = event.modifiers()
+            if key == QtCore.Qt.Key.Key_N:
+                self.fill_table()
+                return True
+            if key == QtCore.Qt.Key.Key_R and mod == QtCore.Qt.KeyboardModifier.ShiftModifier:
+                self.fill_table("red_blind")
+                return True
+            if key == QtCore.Qt.Key.Key_R:
+                self.fill_table("red_weak")
+                return True
+            if key == QtCore.Qt.Key.Key_G and mod == QtCore.Qt.KeyboardModifier.ShiftModifier:
+                self.fill_table("green_blind")
+                return True
+            if key == QtCore.Qt.Key.Key_G:
+                self.fill_table("green weak")
+                return True
+        return False
 
     def color_selected(self):
         """ Get colour selection from table widget. """
@@ -159,14 +250,20 @@ class DialogColorSelect(QtWidgets.QDialog):
         return self.selected_color
 
     def accept(self):
-        """ Overrride accept button. """
+        """ Override accept button. """
 
         super(DialogColorSelect, self).accept()
 
-    def setup_ui(self):
-        """ Eight rows of ten columns of colours. """
+    def fill_table(self, color_range="normal"):
+        """ Twelve rows of ten columns of colours.
+        normal, red weak, red blnd, green weak, green blind
+        param:
+        color_range: String
+        """
 
         self.ui.tableWidget.setSelectionMode(QtWidgets.QAbstractItemView.SelectionMode.SingleSelection)
+        for r in range(self.ui.tableWidget.rowCount()):
+            self.ui.tableWidget.removeRow(0)
         self.ui.tableWidget.setColumnCount(COLS)
         self.ui.tableWidget.setRowCount(ROWS)
         self.ui.tableWidget.verticalHeader().setVisible(False)
@@ -184,7 +281,19 @@ class DialogColorSelect(QtWidgets.QDialog):
                         ttip += c[1] + "\n"
                 item = QtWidgets.QTableWidgetItem(text)
                 item.setToolTip(ttip)
-                item.setBackground(QtGui.QBrush(QtGui.QColor(code_color)))
+                if color_range == "normal":
+                    #print("n", code_color)
+                    item.setBackground(QtGui.QBrush(QtGui.QColor(code_color)))
+                if color_range == "red_weak":
+                    #print("rw", colors_red_weak[row * COLS + col])
+                    item.setBackground(QtGui.QBrush(QtGui.QColor(colors_red_weak[row * COLS + col])))
+                if color_range == "red_blind":
+                    #print("rb", colors_red_blind[row * COLS + col])
+                    item.setBackground(QtGui.QBrush(QtGui.QColor(colors_red_blind[row * COLS + col])))
+                '''if color_range == "green_weak":
+                    item.setBackground(QtGui.QBrush(QtGui.QColor(colors_green_weak[row * COLS + col])))
+                if color_range == "green_blind":
+                    item.setBackground(QtGui.QBrush(QtGui.QColor(colors_green_blind[row * COLS + col])))'''
                 item.setForeground(QtGui.QBrush(QtGui.QColor(TextColor(code_color).recommendation)))
                 item.setFlags(item.flags() ^ QtCore.Qt.ItemFlag.ItemIsEditable)
                 self.ui.tableWidget.setItem(row, col, item)
