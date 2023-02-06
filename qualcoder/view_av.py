69fd50091a5f96a4ef182972434826e9445004f6
QualCoder Developer <ccbogel@hotmail.com>
2023-02-06 21:50:52 +1100

    Add files via upload

--- qualcoder/view_av.py
+++ qualcoder/view_av.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Copyright (c) 2022 Colin Curtain
+Copyright (c) 2023 Colin Curtain
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
