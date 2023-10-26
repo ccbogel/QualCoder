774d24cd59bd4e06ee078f32c208a49cf39cc821
QualCoder Developer <ccbogel@hotmail.com>
2023-10-27 08:43:46 +1100

    update licences information

--- qualcoder/information.py
+++ qualcoder/information.py
@@ -196,7 +196,23 @@ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT<br />\
 HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,<br />\
 WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING<br />\
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR<br />\
-OTHER DEALINGS IN THE SOFTWARE.'
+OTHER DEALINGS IN THE SOFTWARE.\
+<h3>License for wordcloud</h3>\
+Copyright (c) 2012 Andreas Christian Mueller<br />\
+Permission is hereby granted, free of charge, to any person obtaining a copy of<br />\
+this software and associated documentation files (the "Software"), to deal in<br />\
+the Software without restriction, including without limitation the rights to<br />\
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of<br />\
+the Software, and to permit persons to whom the Software is furnished to do so,<br />\
+subject to the following conditions:<br /><br />\
+The above copyright notice and this permission notice shall be included in all<br />\
+copies or substantial portions of the Software.<br /><br />\
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR<br />\
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS<br />\
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR<br />\
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER<br />\
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN<br />\
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.'
 
 menu_shortcuts = '<h1>Menu key shortcuts</h1>\n\
 <table><tr><td><b>Menu shortcuts</b></td><td><b>Project menu shortcuts</b></td>\n\
