9c7973cc574879599d9e8f48c94b51aa0c0e8078
Ingmar Lippert <admin@ingli.de>
2024-02-28 01:30:41 +0100

    Update install_fedora.sh cleaning up the repo command and the file with comments

--- install_fedora.sh
+++ install_fedora.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+printf "This installer is for Fedora Linux installations using python $python_version only.\n"
 
-# first we add the RPM Fusion Free Updates repo, which is a third-party repository!
-sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-39.noarch.rpm
 
+# first we add the RPM Fusion Free Updates repo, which is a third-party repository! We need it for vlc-related files
+sudo dnf install \
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+
+
+# Here we define which packages we install
 dnf_packages="python3-pip python3-devel python3-pdfminer.noarch python3-qt5 python3-pillow python3-openpyxl python3-pandas python3-plotly python3-pip python3-pyqt6 python3-pillow python3-vlc vlc python3-ply python3-six python3-chardet ffmpeg"
 pip_packages="Ebooklib pydub SpeechRecognition pdfminer.six rispy xmlschema"
+
+printf "Change the python_version in this script to a higher numbers if you have a more recent version of python installed.\n\n"
 python_version=3.12
 
-printf "This installer is for Fedora Linux installations using python $python_version only.\n"
-printf "Change the python_version in this script to a higher numbers if you have a more recent version of python installed.\n\n"
 echo "This installer uses DNF package management."
 echo "QualCoder will be copied to the directory /usr/share/"
 echo "These actions require owner (sudo) permission"
@@ -28,7 +33,7 @@ echo "To remove qualcoder from Linux run the following in the terminal:"
 echo "sudo rm -R /usr/share/qualcoder"
 echo "sudo rm /usr/share/icons/qualcoder128.png"
 echo "sudo rm /usr/share/applications/qualcoder.desktop"
-printf "\nAlso note that via dnf the subsequent packages were installed (or used): $dnf_packages.\n\n"
+printf "\nAlso note that via dnf the subsequent packages were installed (or used): $dnf_packages. For that, the `rpmfusion` repository was also enabled. \n\n"
 printf "And via python's pip these packages were installed: $pip_packages.\n\n"
 echo "Consider whether you still need these packages."
 
