92affb7b89fecbf66fc9e9d956d26104698f0c19
QualCoder Developer <ccbogel@hotmail.com>
2024-12-03 08:15:01 +1100

    use qtawesome

--- qualcoder/report_relations.py
+++ qualcoder/report_relations.py
@@ -34,7 +34,6 @@ from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QBrush
 
 from .color_selector import TextColor
-from .GUI.base64_helper import *
 from .GUI.ui_dialog_code_relations import Ui_Dialog_CodeRelations
 from .helpers import DialogCodeInText, ExportDirectoryPathDialog, Message
 from .select_items import DialogSelectItems
