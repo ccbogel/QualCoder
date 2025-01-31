665bf3599998c76e9ebb1457a10108928301abc8
QualCoder Developer <ccbogel@hotmail.com>
2025-01-31 17:37:40 +1100

    Remove save option. The default OS image viewer has a Save option.

--- qualcoder/simple_wordcloud.py
+++ qualcoder/simple_wordcloud.py
@@ -1,25 +1,18 @@
 # -*- coding: utf-8 -*-
 
 """
-Copyright (c) 2023 Colin Curtain
+This file is part of QualCoder.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+QualCoder is free software: you can redistribute it and/or modify it under the
+terms of the GNU Lesser General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later version.
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+QualCoder is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+You should have received a copy of the GNU Lesser General Public License along with QualCoder.
+If not, see <https://www.gnu.org/licenses/>.
 
 Author: Colin Curtain (ccbogel)
 https://github.com/ccbogel/QualCoder
@@ -292,6 +285,7 @@ class Wordcloud:
         temp_filepath = os.path.join(os.path.expanduser("~"), ".qualcoder", f"wordcloud_temp.png")
         img.save(temp_filepath)
         webbrowser.open(temp_filepath)
+        ''' Should Open with default OS image program. No need to have a separate Save below.
         filepath, ok = QtWidgets.QFileDialog.getSaveFileName(None, _("Save wordcloud"),
                                                              self.app.settings['directory'],
                                                              "PNG Files(*.png)")
@@ -300,7 +294,7 @@ class Wordcloud:
             return
         if filepath[-3:] != ".png":
             filepath += ".png"
-        img.save(filepath)
+        img.save(filepath)'''
 
 
 if __name__ == "__main__":
