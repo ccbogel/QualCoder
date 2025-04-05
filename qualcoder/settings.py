59a9423a33e68d321df69fa9c375c981020c4ec1
QualCoder Developer <ccbogel@hotmail.com>
2025-04-06 09:02:29 +1000

    add langauge options ja,sv, zh

--- qualcoder/settings.py
+++ qualcoder/settings.py
@@ -73,7 +73,7 @@ class DialogSettings(QtWidgets.QDialog):
                     coders.append(row[0])
         self.ui.comboBox_coders.addItems(coders)
         languages = ["Deutsch de", "English en", "Español es", "Français fr",
-                     "Italiano it", "Português, pt"]
+                     "Italiano it", "日本語 ja", "Português pt", "svenska sv", "中国人 zh"]
         self.ui.comboBox_language.addItems(languages)
         for index, lang in enumerate(languages):
             if lang[-2:] == self.settings['language']:
