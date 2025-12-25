98568e47d9392e38c187cbee93d07621ada7b7a7
QualCoder Developer <ccbogel@hotmail.com>
2025-12-26 10:53:17 +1100

    Refactor: self.filenames to self.files - more consistent naming across modules

--- src/qualcoder/code_text.py
+++ src/qualcoder/code_text.py
@@ -491,13 +491,14 @@ class DialogCodeText(QtWidgets.QWidget):
         if ids is None:
             ids = []
         self.ui.listWidget.clear()
-        self.filenames = self.app.get_text_filenames(ids)
+        self.files = self.app.get_text_filenames(ids)
         # Fill additional details about each file in the memo
         cur = self.app.conn.cursor()
         sql_length = "select length(fulltext), fulltext from source where id=?"
         sql_codings = "select count(cid) from code_text where fid=? and owner=?"
-        sql_case = "SELECT group_concat(cases.name) from cases join case_text on case_text.caseid=cases.caseid where case_text.fid=?"
-        for file_ in self.filenames:
+        sql_case = "SELECT group_concat(cases.name) from cases join case_text on case_text.caseid=cases.caseid " \
+                   "where case_text.fid=?"
+        for file_ in self.files:
             cur.execute(sql_length, [file_['id'], ])
             res_length = cur.fetchone()
             if res_length is None:  # Safety catch
@@ -523,19 +524,19 @@ class DialogCodeText(QtWidgets.QWidget):
             file_['tooltip'] = tt
         # Sorting the file list
         if sort == "name asc":
-            self.filenames = sorted(self.filenames, key=lambda x: x['name'])
+            self.files = sorted(self.files, key=lambda x: x['name'])
         if sort == "name desc":
-            self.filenames = sorted(self.filenames, key=lambda x: x['name'], reverse=True)
+            self.files = sorted(self.files, key=lambda x: x['name'], reverse=True)
         if sort == "case asc":
-            self.filenames = sorted(self.filenames, key=lambda x: x['case'])
+            self.files = sorted(self.files, key=lambda x: x['case'])
         if sort == "case desc":
-            self.filenames = sorted(self.filenames, key=lambda x: x['case'], reverse=True)
+            self.files = sorted(self.files, key=lambda x: x['case'], reverse=True)
         if sort == "date asc":
-            self.filenames = sorted(self.filenames, key=lambda x: x['date'])
+            self.files = sorted(self.files, key=lambda x: x['date'])
         if sort == "date desc":
-            self.filenames = sorted(self.filenames, key=lambda x: x['date'], reverse=True)
+            self.files = sorted(self.files, key=lambda x: x['date'], reverse=True)
         # Fill list widget
-        for file_ in self.filenames:
+        for file_ in self.files:
             item = QtWidgets.QListWidgetItem(file_['name'])
             item.setToolTip(file_['tooltip'])
             self.ui.listWidget.addItem(item)
@@ -3146,7 +3147,7 @@ class DialogCodeText(QtWidgets.QWidget):
         """ Context menu for listWidget files to get to the next file and
         to go to the file with the latest codings by this coder.
         Sorting files.
-        Each file dictionary item in self.filenames contains:
+        Each file dictionary item in self.files contains:
         {'id', 'name', 'memo', 'characters'= number of characters in the file,
         'start' = showing characters from this position, 'end' = showing characters to this position}
 
@@ -3157,7 +3158,7 @@ class DialogCodeText(QtWidgets.QWidget):
         selected = self.ui.listWidget.currentItem()
         if not selected:
             return
-        file_ = next((f for f in self.filenames if f['name'] == selected.text()), None)
+        file_ = next((f for f in self.files if f['name'] == selected.text()), None)
         menu = QtWidgets.QMenu()
         menu.setStyleSheet(f"QMenu {{font-size:{self.app.settings['fontsize']}pt}} ")
         action_next = None
@@ -3173,7 +3174,7 @@ class DialogCodeText(QtWidgets.QWidget):
                 (file_['mediapath'][:6] == '/docs/' or file_['mediapath'][:5] == 'docs:'):
             action_view_original_text = menu.addAction(_("view original text file"))
         if len(self.app.get_text_filenames()) > 1:
-            if len(self.filenames) != 1:
+            if len(self.files) != 1:
                 action_next = menu.addAction(_("Next file"))
             action_latest = menu.addAction(_("File with latest coding"))
             action_show_files_like = menu.addAction(_("Show files like"))
@@ -3390,12 +3391,12 @@ class DialogCodeText(QtWidgets.QWidget):
         """ Go to next file in list. Button. """
 
         if self.file_ is None:
-            self.load_file(self.filenames[0])
+            self.load_file(self.files[0])
             self.ui.listWidget.setCurrentRow(0)
             return
-        for i in range(0, len(self.filenames) - 1):
-            if self.file_ == self.filenames[i]:
-                found = self.filenames[i + 1]
+        for i in range(0, len(self.files) - 1):
+            if self.file_ == self.files[i]:
+                found = self.files[i + 1]
                 self.ui.listWidget.setCurrentRow(i + 1)
                 self.load_file(found)
                 self.search_term = ""
@@ -3410,7 +3411,7 @@ class DialogCodeText(QtWidgets.QWidget):
         result = cur.fetchone()
         if result is None:
             return
-        for i, f in enumerate(self.filenames):
+        for i, f in enumerate(self.files):
             if f['id'] == result[0]:
                 self.ui.listWidget.setCurrentRow(i)
                 self.load_file(f)
@@ -3429,7 +3430,7 @@ class DialogCodeText(QtWidgets.QWidget):
         cur = self.app.conn.cursor()
         cur.execute("select bookmarkfile, bookmarkpos from project")
         result = cur.fetchone()
-        for i, f in enumerate(self.filenames):
+        for i, f in enumerate(self.files):
             if f['id'] == result[0]:
                 f['start'] = 0
                 if f['end'] != f['characters']:
@@ -3465,10 +3466,10 @@ class DialogCodeText(QtWidgets.QWidget):
         The selected file is then displayed for coding.
         Note: file segment is also loaded from listWidget context menu """
 
-        if len(self.filenames) == 0:
+        if len(self.files) == 0:
             return
         item_name = self.ui.listWidget.currentItem().text()
-        for f in self.filenames:
+        for f in self.files:
             if f['name'] == item_name:
                 self.file_ = f
                 self.load_file(self.file_)
@@ -3479,7 +3480,7 @@ class DialogCodeText(QtWidgets.QWidget):
         """ File selection changed. """
 
         row = self.ui.listWidget.currentRow()
-        self.load_file(self.filenames[row])
+        self.load_file(self.files[row])
 
     def load_file(self, file_):
         """ Load and display file text for this file.
@@ -3974,7 +3975,7 @@ class DialogCodeText(QtWidgets.QWidget):
             Message(self.app, _('Warning'), _("Cannot have blank text marks"), "warning").exec()
             return
 
-        ui = DialogSelectItems(self.app, self.filenames, _("Select files to code"), "many")
+        ui = DialogSelectItems(self.app, self.files, _("Select files to code"), "many")
         ok = ui.exec()
         if not ok:
             return
@@ -4086,7 +4087,7 @@ class DialogCodeText(QtWidgets.QWidget):
         Button Right-click options are: all (default), first, last, code within code.
         """
 
-        ui = DialogSelectItems(self.app, self.filenames, _("Select files to code"), "many")
+        ui = DialogSelectItems(self.app, self.files, _("Select files to code"), "many")
         ok = ui.exec()
         if not ok:
             return
@@ -4254,9 +4255,9 @@ class DialogCodeText(QtWidgets.QWidget):
         # Regex, pipe | has different meaning, so do not split into separate texts
         if self.ui.checkBox_auto_regex.isChecked():
             find_texts = [find_text]
-        if len(self.filenames) == 0:
+        if len(self.files) == 0:
             return
-        ui = DialogSelectItems(self.app, self.filenames, _("Select files to code"), "many")
+        ui = DialogSelectItems(self.app, self.files, _("Select files to code"), "many")
         ok = ui.exec()
         if not ok:
             return
@@ -5331,7 +5332,7 @@ class DialogCodeText(QtWidgets.QWidget):
     def open_doc_selection(self, doc_id, sel_start, sel_end):
         """ Open document and select a certain part. """
 
-        for i, f in enumerate(self.filenames):
+        for i, f in enumerate(self.files):
             if f['id'] == doc_id:
                 f['start'] = 0
                 if f['end'] != f['characters']:  # partially loaded
