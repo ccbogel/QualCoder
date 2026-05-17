353f2e24f302b8e605d68b03e9d75bd7ae5cac64
QualCoder Developer <ccbogel@hotmail.com>
2026-05-17 17:53:27 +1000

    Run from source scripts

---
+++ run_from_source_Ubuntu.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Make this script executable
+# Run it by typing inside the Qualcoder folder: ./Ubuntu_run_from_source.sh
+
+echo "Starting"
+# Create venv
+if [ -d ".env"]; then
+    echo "Virtual environment exists."
+else
+    echo "Creating virtual environment."
+    python3 -m venv ".env"
+fi
+
+# Activate environment
+source .env/bin/activate
+
+# Install required modules
+echo "Installing requirements. This may take 10 minutes."
+python3 -m pip install --upgrade pip
+pip install -r requirements.txt
+
+# Move to src folder to then run qualcoder module
+echo "Starting QualCoder."
+cd src
+python3 -m qualcoder
+
+# Exit environment
+cd ../
+deactivate
+echo "Exiting virtual environment."
