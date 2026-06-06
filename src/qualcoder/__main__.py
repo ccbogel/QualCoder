cbc6183b19bef51e6a4aafbf9891cf136775e223
Dr. Justin Poncet <justinmponcet@missaghiehponcet.eu>
2026-06-06 19:13:39 +0200

    enhance i18n

--- src/qualcoder/__main__.py
+++ src/qualcoder/__main__.py
@@ -56,7 +56,7 @@ from qualcoder.GUI.ui_main import Ui_MainWindow
 from qualcoder.helpers import Message, ImportPlainTextCodes
 from qualcoder.import_survey import DialogImportSurvey
 from qualcoder.information import DialogInformation, menu_shortcuts_display, coding_shortcuts_display
-from qualcoder.locale.base64_lang_helper import *
+from qualcoder.i18n.base64_lang_helper import *
 from qualcoder.journals import DialogJournals
 from qualcoder.manage_files import DialogManageFiles
 from qualcoder.manage_links import DialogManageLinks
