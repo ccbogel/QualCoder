0037b73235c9dd5f6aaa392d14355d061895f722
QualCoder Developer <ccbogel@hotmail.com>
2026-07-29 15:02:01 +1000

    remove os

--- src/qualcoder/report_cooccurrence.py
+++ src/qualcoder/report_cooccurrence.py
@@ -14,7 +14,7 @@ See the GNU General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License along with QualCoder.
 If not, see <https://www.gnu.org/licenses/>.
 
-Author: Colin Curtain (ccbogel)
+Authors: Colin Curtain C, Kai Dröge, Justin Missaghieh--Poncet, Lorenzo Salomón
 https://github.com/ccbogel/QualCoder
 https://qualcoder.wordpress.com/
 https://qualcoder-org.github.io
@@ -28,7 +28,6 @@ import logging
 import openpyxl
 from openpyxl.utils import get_column_letter
 from openpyxl.styles import PatternFill
-import os
 import sqlite3
 import qtawesome as qta  # see: https://pictogrammers.com/library/mdi/
 
@@ -39,7 +38,6 @@ from .helpers import ExportDirectoryPathDialog, Message
 from .report_attributes import DialogSelectAttributeParameters
 from .select_items import DialogSelectItems
 
-path = os.path.abspath(os.path.dirname(__file__))
 logger = logging.getLogger(__name__)
 
 # --- Imports for static image export
