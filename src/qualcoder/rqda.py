e8e0fe1bded2d13ea3c124ebbbb85092385c0011
QualCoder Developer <ccbogel@hotmail.com>
2026-07-29 15:24:21 +1000

    removed os

--- src/qualcoder/rqda.py
+++ src/qualcoder/rqda.py
@@ -14,7 +14,7 @@ See the GNU General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License along with QualCoder.
 If not, see <https://www.gnu.org/licenses/>.
 
-Author: Colin Curtain (ccbogel)
+Authors: Colin Curtain C, Kai Dröge, Justin Missaghieh--Poncet, Lorenzo Salomón
 https://github.com/ccbogel/QualCoder
 https://qualcoder.wordpress.com/
 https://qualcoder-org.github.io
@@ -23,7 +23,6 @@ https://qualcoder.org/
 
 import datetime
 import logging
-import os
 from random import randint
 import sqlite3
 
@@ -31,7 +30,6 @@ from PyQt6 import QtWidgets
 
 from .color_selector import colors
 
-path = os.path.abspath(os.path.dirname(__file__))
 logger = logging.getLogger(__name__)
 
 
