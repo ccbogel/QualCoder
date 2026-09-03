b6951a7968f5be9324bd660b6d7e061559949af2
Dr. Justin Poncet <justinmponcet@missaghiehponcet.eu>
2026-09-03 18:43:34 +0200

    compile with upx

--- debian_build-deb.sh
+++ debian_build-deb.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# sudo aptitude install dpkg-dev fakeroot upx-ucl
 # set -e
 
 if [ -z "$1" ]; then
@@ -14,7 +15,7 @@ python3 -m venv ".env"
 source .env/bin/activate
 python3 -m pip install --upgrade pip
 pip install -r requirements.txt
-pyinstaller qualcoder_onefile.spec
+pyinstaller qualcoder_onefile.spec --upx-dir=/usr/bin/upx
 deactivate
 
 #  Create .deb
