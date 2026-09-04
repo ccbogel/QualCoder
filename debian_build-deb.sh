6dae6231f0c2a53af3cd74bc497bfe3ce7e6e2c5
J. aka Qwerty <justinmponcet@missaghiehponcet.eu>
2026-09-04 10:20:11 +0200

    Update debian_build-deb.sh

--- debian_build-deb.sh
+++ debian_build-deb.sh
@@ -1,4 +1,78 @@
 #!/bin/bash
+# set -e
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <version> (ex: 3.8.3)"
+    exit 1
+fi
+
+VERSION="$1"
+NEW_DIR="qualcoder-${VERSION}"
+
+# Compile QualCoder
+python3 -m venv ".env"
+source .env/bin/activate
+python3 -m pip install --upgrade pip
+pip install -r requirements.txt
+pyinstaller qualcoder_onefile.spec
+deactivate
+
+#  Create .deb
+echo "Copy template ${NEW_DIR}..."
+cp -r "qualcoder-debian" "$NEW_DIR"
+
+mkdir -p "${NEW_DIR}/src"
+cp "dist/QualCoder" "${NEW_DIR}/src/qualcoder"
+chmod +x "${NEW_DIR}/src/qualcoder"
+
+
+echo "Update changelog ..."
+cat > "${NEW_DIR}/debian/changelog" << EOF
+qualcoder (${VERSION}) UNRELEASED; urgency=medium
+
+  * New upstream release ${VERSION}.
+  * See https://github.com/ccbogel/QualCoder/releases/tag/${VERSION}
+
+ -- QualCoder Team <contact@qualcoder.org>  $(date -R)
+EOF
+
+sed -i "s/Standards-Version:.*/Standards-Version: ${VERSION}/" "${NEW_DIR}/debian/control"
+
+mkdir -p "${NEW_DIR}/usr/share/man/man1"
+cat > "${NEW_DIR}/usr/share/man/man1/qualcoder.1" << 'EOF'
+.TH QUALCODER 1
+.SH NAME
+qualcoder \- Qualitative Data Analysis Tool
+.SH SYNOPSIS
+.B qualcoder
+.SH DESCRIPTION
+QualCoder is a free and open-source Computer Assisted Qualitative Data Analysis Software (CAQDAS) for coding qualitative data such as text, images, and audio/video.
+.SH OPTIONS
+There are no command-line options. QualCoder is a graphical application.
+.SH AUTHOR
+QualCoder Team <contact@qualcoder.org>
+.SH SEE ALSO
+.BR https://github.com/ccbogel/QualCoder
+EOF
+
+cat > "${NEW_DIR}/debian/rules" << 'EOF'
+#!/usr/bin/make -f
+%:
+    dh $@
+
+override_dh_auto_install:
+    install -D -m 755 src/qualcoder debian/qualcoder/usr/bin/qualcoder
+    install -D -m 644 usr/share/applications/QualCoder.desktop debian/qualcoder/usr/share/applications/QualCoder.desktop
+    install -D -m 644 usr/share/icons/qualcoder.xpm debian/qualcoder/usr/share/icons/qualcoder.xpm
+    install -D -m 644 usr/share/man/man1/qualcoder.1 debian/qualcoder/usr/share/man/man1/qualcoder.1
+EOF
+
+echo "Build version ${VERSION}..."
+cd "$NEW_DIR"
+debuild -us -uc
+cd ..
+
+echo "Package released : qualcoder_${VERSION}_amd64.deb"#!/bin/bash
 set -e  # Exit on first error
 
 # Check for required dependencies
